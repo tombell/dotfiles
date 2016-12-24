5c6d20a8bce4e6f354b3b717bece4363d26a088b
Tom Bell <tomb@tomb.io>
2016-12-24 04:59:46 +0000

    Add zshrc

---
+++ zsh/configs/post/path.zsh
@@ -0,0 +1,18 @@
+# mkdir .git/safe in the root of repositories you trust
+PATH=".git/safe/../../bin:$PATH"
+
+# load rbenv if available
+if command -v rbenv >/dev/null; then
+  eval "$(rbenv init - --no-rehash)"
+fi
+
+# load nodenv if available
+if command -v nodenv >/dev/null; then
+  eval "$(nodenv init - --no-rehash)"
+fi
+
+GOPATH=$PROJECTS/go
+
+PATH="$(brew --prefix go)/libexec/bin:$GOPATH/bin:$PATH"
+
+export -U PATH GOPATH
---
+++ zsh/configs/prompt.zsh
@@ -0,0 +1,8 @@
+git_prompt_info() {
+  current_branch=$(git rev-parse --abbrev-ref HEAD 2> /dev/null)
+  if [[ -n $current_branch ]]; then
+    echo "%{$fg_bold[black]%}$current_branch%{$reset_color%} "
+  fi
+}
+setopt promptsubst
+PS1='%{$fg_bold[yellow]%}%c $(git_prompt_info)%{$fg_bold[blue]%}▸%{$reset_color%} '
