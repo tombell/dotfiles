8658ea666771e01bcba698350ad7f1082c06a827
Tom Bell <tomb@tomb.io>
2018-01-29 12:49:23 +0000

    Remove .git/safe bin path

--- zsh/configs/post/path.zsh
+++ zsh/configs/post/path.zsh
@@ -1,6 +1,3 @@
-# mkdir .git/safe in the root of repositories you trust
-PATH=".git/safe/../../bin:$PATH"
-
 # load rbenv if available
 if command -v rbenv >/dev/null; then
   eval "$(rbenv init - --no-rehash)"
