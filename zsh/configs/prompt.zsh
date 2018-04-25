6c17ef8036f59f5166f3b4bc50b7d18f292fb0b0
Tom Bell <tomb@tomb.io>
2018-04-26 00:42:16 +0100

    Try out new prompt

--- zsh/configs/prompt.zsh
+++ zsh/configs/prompt.zsh
@@ -1,8 +1,8 @@
 git_prompt_info() {
   current_branch=$(git rev-parse --abbrev-ref HEAD 2> /dev/null)
   if [[ -n $current_branch ]]; then
-    echo "%{$fg[blue]%}$current_branch%{$reset_color%} "
+    echo "%{$fg[blue]%}($current_branch)%{$reset_color%} "
   fi
 }
 setopt promptsubst
-PS1='%{$fg[yellow]%}%c %{$reset_color%}$(git_prompt_info)%{$fg[green]%}▸%{$reset_color%} '
+PS1='%{$fg[yellow]%}%~ %{$reset_color%}$(git_prompt_info)%{$fg[green]%}▸%{$reset_color%} '
