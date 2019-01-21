4eff39258645d54368e55b2c0ea3db5c772f3314
Tom Bell <tomb@tomb.io>
2019-01-21 02:02:34 +0000

    Clean up PATH and remove old paths

--- zsh/configs/post/path.zsh
+++ zsh/configs/post/path.zsh
@@ -1,14 +1,9 @@
-PATH="/usr/local/opt/curl/bin:$PATH"
-PATH="/usr/local/opt/mysql@5.7/bin:$PATH"
 PATH="/usr/local/opt/openssl/bin:$PATH"
-PATH="/usr/local/opt/sqlite/bin:$PATH"
 
-# load rbenv if available
 if command -v rbenv >/dev/null; then
   eval "$(rbenv init - --no-rehash)"
 fi
 
-# load nodenv if available
 if command -v nodenv >/dev/null; then
   eval "$(nodenv init - --no-rehash)"
 fi
