6cc8f49e508e8d6f680ecd2f247029baf46a54c6
Tom Bell <tomb@tomb.io>
2019-09-07 02:32:49 +0100

    Remove Android PATH configuration

--- zsh/configs/post/path.zsh
+++ zsh/configs/post/path.zsh
@@ -1,4 +1,3 @@
-PATH="./node_modules/.bin:$PATH"
 
 if command -v rbenv >/dev/null; then
   eval "$(rbenv init - --no-rehash)"
@@ -8,9 +7,7 @@ if command -v nodenv >/dev/null; then
   eval "$(nodenv init - --no-rehash)"
 fi
 
-ANDROID_SDK_ROOT=$HOME/Library/Android/sdk
-
 GOPATH=$HOME/.go
-PATH="$GOPATH/bin:$HOME/Library/Android/sdk/platform-tools:$PATH:"
+PATH="$GOPATH/bin:./node_modules/.bin:$PATH"
 
-export -U PATH GOPATH ANDROID_SDK_ROOT
+export -U GOPATH PATH
