7cf80d0d34e559f26d3891e18f3524f15644017b
Tom Bell <tomb@tomb.io>
2020-07-09 15:59:55 +0100

    Update fzf.zsh to load installed configuration

--- zsh/configs/fzf.zsh
+++ zsh/configs/fzf.zsh
@@ -1,4 +1,6 @@
 # Set the default FZF command to use ripgrep
 FZF_DEFAULT_COMMAND='rg --files --hidden --follow --glob "!.git/*"'
 
+[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+
 export -U FZF_DEFAULT_COMMAND
