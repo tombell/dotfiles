c2a79f8c7347e966ef35672303bc2aed113cbde9
Tom Bell <tomb@tomb.io>
2020-09-23 10:00:25 +0100

    Nicer formatting

--- bin/note-current-track.sh
+++ bin/note-current-track.sh
@@ -2,11 +2,11 @@
 set -e
 
 function main {
-  /usr/bin/osascript \
-    -e 'tell application "Music"' \
+  /usr/bin/osascript                                   \
+    -e 'tell application "Music"'                      \
     -e '  set track_artist to artist of current track' \
-    -e '  set track_name to name of current track' \
-    -e '  log track_artist & " - " & track_name' \
+    -e '  set track_name to name of current track'     \
+    -e '  log track_artist & " - " & track_name'       \
     -e 'end tell' 2>> "$HOME/Desktop/tracks.txt"
 }
 
