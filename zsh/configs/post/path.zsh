f14da3fecf966821484ce6bc3d4c4b6d2cb16884
Tom Bell <tomb@tomb.io>
2020-11-30 09:40:23 +0000

    No longer need $GOPATH

--- zsh/configs/post/path.zsh
+++ zsh/configs/post/path.zsh
@@ -6,10 +6,8 @@ if command -v nodenv >/dev/null; then
   eval "$(nodenv init - --no-rehash)"
 fi
 
-GOPATH=$HOME/.go
-
 PATH="$HOME/.cargo/bin:$PATH"
 PATH="$GOPATH/bin:$PATH"
 PATH="./node_modules/.bin:$PATH"
 
-export -U GOPATH PATH
+export -U PATH
