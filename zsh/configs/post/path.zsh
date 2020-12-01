e328acd265665ffa7d2435353fc5c06a61aca1c2
Tom Bell <tomb@tomb.io>
2020-12-01 22:44:56 +0000

    Revert "No longer need $GOPATH"
    This reverts commit f14da3fecf966821484ce6bc3d4c4b6d2cb16884.

--- zsh/configs/post/path.zsh
+++ zsh/configs/post/path.zsh
@@ -6,7 +6,9 @@ if command -v nodenv >/dev/null; then
   eval "$(nodenv init - --no-rehash)"
 fi
 
+GOPATH=$HOME/.go
+
 PATH="$GOPATH/bin:$PATH"
 PATH="./node_modules/.bin:$PATH"
 
-export -U PATH
+export -U GOPATH PATH
