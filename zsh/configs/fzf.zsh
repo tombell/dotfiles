b1c1b0db38c3fbf4047b6c3abd8c73d5581da3d2
Tom Bell <tomb@tomb.io>
2020-12-11 14:43:59 +0000

    Re-enable

--- zsh/configs/fzf.zsh
+++ zsh/configs/fzf.zsh
@@ -5,10 +5,10 @@ FZF_DEFAULT_COMMAND='rg --files --hidden --follow --glob "!.git/*"'
 FZF_DEFAULT_OPTS='--height 25%'
 
 # Set the FZF color scheme to dracula
-# FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS'
-# --color=dark
-# --color=fg:-1,bg:-1,hl:#5fff87,fg+:-1,bg+:-1,hl+:#ffaf5f
-# --color=info:#af87ff,prompt:#5fff87,pointer:#ff87d7,marker:#ff87d7,spinner:#ff87d7
-# '
+FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS'
+--color=dark
+--color=fg:-1,bg:-1,hl:#5fff87,fg+:-1,bg+:-1,hl+:#ffaf5f
+--color=info:#af87ff,prompt:#5fff87,pointer:#ff87d7,marker:#ff87d7,spinner:#ff87d7
+'
 
 export -U FZF_DEFAULT_COMMAND FZF_DEFAULT_OPTS
