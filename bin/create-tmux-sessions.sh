49e998efb9a370d40984a5de0c87727b13efe902
Tom Bell <tomb@tomb.io>
2021-01-15 09:38:49 +0000

    Remove key-chain from create tmux sessions script

--- bin/create-tmux-sessions.sh
+++ bin/create-tmux-sessions.sh
@@ -5,7 +5,6 @@ cd ~/Code/backstage   && tmux new-session -d -s backstage
 cd ~/Code/doorbell    && tmux new-session -d -s doorbell
 cd ~/Code/foundations && tmux new-session -d -s foundations
 cd ~/Code/frontdoor   && tmux new-session -d -s frontdoor
-cd ~/Code/key-chain   && tmux new-session -d -s key-chain
 cd ~/Code/letterbox   && tmux new-session -d -s letterbox
 cd ~/Code/living      && tmux new-session -d -s living
 cd ~/Code/marketing   && tmux new-session -d -s marketing
