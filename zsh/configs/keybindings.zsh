2d780aa129c0c90bc50a9126f32fc511aff93dbb
Tom Bell <tomb@tomb.io>
2021-01-15 09:45:12 +0000

    Comment out zsh binds I do not think I use

--- zsh/configs/keybindings.zsh
+++ zsh/configs/keybindings.zsh
@@ -11,7 +11,6 @@ bindkey "^E" end-of-line
 bindkey "^K" kill-line
 bindkey "^R" history-incremental-search-backward
 bindkey "^P" history-search-backward
-bindkey "^Y" accept-and-hold
-bindkey "^N" insert-last-word
-bindkey "^Q" push-line-or-edit
-# bindkey -s "^T" "^[Isudo ^[A" # "t" for "toughguy"
+# bindkey "^Y" accept-and-hold
+# bindkey "^N" insert-last-word
+# bindkey "^Q" push-line-or-edit
