d733ed860f68ddce284a7da2454f84c313245995
Tom Bell <tomb@tomb.io>
2021-01-18 22:09:35 +0000

    Update toggle-mic.sh script

--- bin/toggle-mic.sh
+++ bin/toggle-mic.sh
@@ -1,18 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
-# Comment out lines 6, 7, 9, and 18 if not using aggregate device
-
-mic_device="Bose QuietComfort 35 Series II"
-aggregate_device="Mic"
-
-SwitchAudioSource -t input -s "$mic_device"
-
 osascript \
   -e 'if input volume of (get volume settings) = 0 then' \
   -e '  set volume input volume 50' \
   -e 'else' \
   -e '  set volume input volume 0' \
   -e 'end if'
-
-SwitchAudioSource -t input -s "$aggregate_device"
