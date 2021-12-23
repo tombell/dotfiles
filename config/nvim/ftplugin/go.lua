f9a2a31ded571505f577d82251061588ad08f161
Tom Bell <tomb@tomb.io>
2021-12-23 00:59:49 +0000

    Refactor ftplugin/go.vim to go.lua

---
+++ config/nvim/ftplugin/go.lua
@@ -0,0 +1,24 @@
+vim.bo.tabstop = 4
+vim.bo.shiftwidth = 4
+vim.bo.softtabstop = 4
+vim.bo.expandtab = false
+
+vim.g.go_highlight_types = 1
+vim.g.go_highlight_fields = 0
+vim.g.go_highlight_functions = 1
+vim.g.go_highlight_function_calls = 1
+vim.g.go_highlight_operators = 1
+vim.g.go_highlight_extra_types = 1
+vim.g.go_highlight_build_constraints = 1
+vim.g.go_highlight_generate_tags = 1
+
+vim.g.go_template_autocreate = 0
+
+vim.g.go_fmt_command = 'goimports'
+
+vim.g.go_jump_to_error = 0
+
+vim.g.go_metalinter_command = 'golangci-lint'
+vim.g.go_metalinter_autosave = 1
+
+vim.cmd [[autocmd BufEnter *.go nmap <leader>r  <Plug>(go-rename)]]
