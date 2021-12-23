52345ce0cd34933763922abf7b892adec176b2a9
Tom Bell <tomb@tomb.io>
2021-12-23 01:22:31 +0000

    Remove vi-cmd-mode and kill-line

--- zsh/configs/keybindings.zsh
+++ zsh/configs/keybindings.zsh
@@ -1,13 +1,8 @@
 # Give us access to ^Q
 stty -ixon
 
-# Vi mode
-bindkey -v
-bindkey "^F" vi-cmd-mode
-
 # Handy keybindings
 bindkey "^A" beginning-of-line
 bindkey "^E" end-of-line
-bindkey "^K" kill-line
 bindkey "^R" history-incremental-search-backward
 bindkey "^P" history-search-backward
