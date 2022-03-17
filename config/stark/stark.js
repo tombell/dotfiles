e2017b9c23e2f96765c30699cc96f6ba9304e089
Tom Bell <tomb@tomb.io>
2022-03-17 22:38:22 +0000

    Update stark.js to use optional chaining

--- config/stark/stark.js
+++ config/stark/stark.js
@@ -10,7 +10,7 @@ const GRID_WIDTH = 12;
 const GRID_HEIGHT = 10;
 
 // Margin to have around each window edge
-const MARGIN = 14;
+const MARGIN = 7;
 
 // -----------------------------------------------------------------------------
 // WINDOW CENTERING
@@ -18,50 +18,37 @@ const MARGIN = 14;
 
 Bind.on("z", MODIFIERS, () => {
   const win = Window.focused();
+  const { width, height, y } = win?.screen.frameWithoutDockOrMenu;
 
-  if (!win) return;
-
-  const r = win.screen.frameWithoutDockOrMenu;
-
-  win.setFrame({
-    x: (r.width / GRID_WIDTH) * 2,
-    y: r.y + (r.height / GRID_HEIGHT) * 2,
-    width: (r.width / GRID_WIDTH) * 8,
-    height: (r.height / GRID_HEIGHT) * 6,
+  win?.setFrame({
+    x: (width / GRID_WIDTH) * 2,
+    y: y + (height / GRID_HEIGHT) * 2,
+    width: (width / GRID_WIDTH) * 8,
+    height: (height / GRID_HEIGHT) * 6,
   });
 });
 
 Bind.on("x", MODIFIERS, () => {
   const win = Window.focused();
+  const { width, height, y } = win?.screen.frameWithoutDockOrMenu;
 
-  if (!win) return;
-
-  const r = win.screen.frameWithoutDockOrMenu;
-
-  win.setFrame({
-    x: r.width / GRID_WIDTH,
-    y: r.y + r.height / GRID_HEIGHT,
-    width: (r.width / GRID_WIDTH) * 10,
-    height: (r.height / GRID_HEIGHT) * 8,
+  win?.setFrame({
+    x: width / GRID_WIDTH,
+    y: y + height / GRID_HEIGHT,
+    width: (width / GRID_WIDTH) * 10,
+    height: (height / GRID_HEIGHT) * 8,
   });
 });
 
 Bind.on("c", MODIFIERS, () => {
   const win = Window.focused();
+  const { width, height, x, y } = win?.screen.frameWithoutDockOrMenu;
 
-  if (!win) return;
-
-  const r = win.screen.frameWithoutDockOrMenu;
-  const x = r.x + MARGIN;
-  const y = r.y + MARGIN;
-  const width = r.width - MARGIN * 2;
-  const height = r.height - MARGIN * 2;
-
-  win.setFrame({
-    x: r.x + MARGIN,
-    y: r.y + MARGIN,
-    width: r.width - MARGIN * 2,
-    height: r.height - MARGIN * 2,
+  win?.setFrame({
+    x: x + MARGIN,
+    y: y + MARGIN,
+    width: width - MARGIN * 2,
+    height: height - MARGIN * 2,
   });
 });
 
@@ -71,31 +58,25 @@ Bind.on("c", MODIFIERS, () => {
 
 Bind.on("h", MODIFIERS, () => {
   const win = Window.focused();
+  const { width, height, x, y } = win?.screen.frameWithoutDockOrMenu;
 
-  if (!win) return;
-
-  const r = win.screen.frameWithoutDockOrMenu;
-
-  win.setFrame({
-    x: r.x + MARGIN,
-    y: r.y + MARGIN,
-    width: r.width / 2 - (MARGIN + MARGIN / 2),
-    height: r.height - MARGIN * 2,
+  win?.setFrame({
+    x: x + MARGIN,
+    y: y + MARGIN,
+    width: width / 2 - (MARGIN + MARGIN / 2),
+    height: height - MARGIN * 2,
   });
 });
 
 Bind.on("l", MODIFIERS, () => {
   const win = Window.focused();
+  const { width, height, x, y } = win?.screen.frameWithoutDockOrMenu;
 
-  if (!win) return;
-
-  const r = win.screen.frameWithoutDockOrMenu;
-
-  win.setFrame({
-    x: r.x + r.width / 2 + MARGIN / 2,
-    y: r.y + MARGIN,
-    width: r.width / 2 - (MARGIN + MARGIN / 2),
-    height: r.height - MARGIN * 2,
+  win?.setFrame({
+    x: x + width / 2 + MARGIN / 2,
+    y: y + MARGIN,
+    width: width / 2 - (MARGIN + MARGIN / 2),
+    height: height - MARGIN * 2,
   });
 });
 
