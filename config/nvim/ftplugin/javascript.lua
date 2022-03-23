5334cbeb0cb4bf8c0afa04587b0c2d62d088462f
Tom Bell <tomb@tomb.io>
2022-03-23 16:18:31 +0000

    Update maps in typescript/javascript ftplugins

--- config/nvim/ftplugin/javascript.lua
+++ config/nvim/ftplugin/javascript.lua
@@ -1,9 +1,7 @@
-opts = { noremap = false, silent = true }
-
-vim.api.nvim_set_keymap('n', 'gd', '<Plug>(coc-definition)', opts)
-vim.api.nvim_set_keymap('n', 'gy', '<Plug>(coc-type-definition)', opts)
-vim.api.nvim_set_keymap('n', 'gi', '<Plug>(coc-implementation)', opts)
-vim.api.nvim_set_keymap('n', 'gr', '<Plug>(coc-references)', opts)
+vim.api.nvim_set_keymap('n', 'gd', '<Plug>(coc-definition)', { noremap = false, silent = true })
+vim.api.nvim_set_keymap('n', 'gy', '<Plug>(coc-type-definition)', { noremap = false, silent = true })
+vim.api.nvim_set_keymap('n', 'gi', '<Plug>(coc-implementation)', { noremap = false, silent = true })
+vim.api.nvim_set_keymap('n', 'gr', '<Plug>(coc-references)', { noremap = false, silent = true })
 
 vim.api.nvim_set_keymap('n', '<leader>r', '<Plug>(coc-rename)', { noremap = false })
 vim.api.nvim_set_keymap('n', 'qf', '<Plug>(coc-fix-current)', { noremap = false })
--- config/nvim/ftplugin/typescript.lua
+++ config/nvim/ftplugin/typescript.lua
@@ -1,21 +1,7 @@
-opts = { noremap = false, silent = true }
-
-vim.api.nvim_set_keymap('n', 'gd', '<Plug>(coc-definition)', opts)
-vim.api.nvim_set_keymap('n', 'gy', '<Plug>(coc-type-definition)', opts)
-vim.api.nvim_set_keymap('n', 'gi', '<Plug>(coc-implementation)', opts)
-vim.api.nvim_set_keymap('n', 'gr', '<Plug>(coc-references)', opts)
+vim.api.nvim_set_keymap('n', 'gd', '<Plug>(coc-definition)', { noremap = false, silent = true })
+vim.api.nvim_set_keymap('n', 'gy', '<Plug>(coc-type-definition)', { noremap = false, silent = true })
+vim.api.nvim_set_keymap('n', 'gi', '<Plug>(coc-implementation)', { noremap = false, silent = true })
+vim.api.nvim_set_keymap('n', 'gr', '<Plug>(coc-references)', { noremap = false, silent = true })
 
 vim.api.nvim_set_keymap('n', '<leader>r', '<Plug>(coc-rename)', { noremap = false })
 vim.api.nvim_set_keymap('n', 'qf', '<Plug>(coc-fix-current)', { noremap = false })
-
-vim.cmd [[
-  function! s:check_back_space() abort
-    let col = col('.') - 1
-    return !col || getline('.')[col - 1]  =~ '\s'
-  endfunction
-
-  inoremap <silent><expr> <Tab>
-    \ pumvisible() ? "\<C-n>" :
-    \ <SID>check_back_space() ? "\<Tab>" :
-    \ coc#refresh()
-]]
