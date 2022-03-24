1202bf4d679fab82a5db61e8f42e4ec176c1bbb3
Tom Bell <tomb@tomb.io>
2022-03-24 17:25:41 +0000

    wip

--- config/nvim/ftplugin/go.lua
+++ config/nvim/ftplugin/go.lua
@@ -3,16 +3,13 @@ vim.bo.shiftwidth = 4
 vim.bo.softtabstop = 4
 vim.bo.expandtab = false
 
-vim.g.go_highlight_types = 1
-vim.g.go_highlight_fields = 0
-vim.g.go_highlight_functions = 1
-vim.g.go_highlight_function_calls = 1
-vim.g.go_highlight_operators = 1
-vim.g.go_highlight_extra_types = 1
-vim.g.go_highlight_build_constraints = 1
-vim.g.go_highlight_generate_tags = 1
-
 vim.g.go_template_autocreate = 0
+
+vim.g.go_highlight_operators = 1
+vim.g.go_highlight_functions = 1
+vim.g.go_highlight_function_parameters = 1
+vim.g.go_highlight_function_calls = 1
+vim.g.go_highlight_types = 1
 
 vim.g.go_fmt_command = 'goimports'
 
