594004c6d9def16569adc38314fb59b581e74a01
Tom Bell <tomb@tomb.io>
2022-04-11 02:26:04 +0100

    Update fzf.zsh to use catppuccin colours

--- config/zsh/configs/fzf.zsh
+++ config/zsh/configs/fzf.zsh
@@ -5,10 +5,10 @@ FZF_DEFAULT_COMMAND='rg --files --hidden --follow --glob "!.git/*"'
 FZF_DEFAULT_OPTS='--height 20%'
 
 # Set the FZF color scheme to dracula
-FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS'
---color=dark
---color=fg:-1,bg:-1,hl:#9ed072,fg+:-1,bg+:-1,hl+:#f39660
---color=info:#b39df3,prompt:#9ed072,pointer:#fc5d7c,marker:#fc5d7c,spinner:#fc5d7c
+export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS'
+--color=bg+:#302D41,bg:#1E1E2E,spinner:#F8BD96,hl:#F28FAD
+--color=fg:#D9E0EE,header:#F28FAD,info:#DDB6F2,pointer:#F8BD96
+--color=marker:#F8BD96,fg+:#F2CDCD,prompt:#DDB6F2,hl+:#F28FAD
 '
 
 export -U FZF_DEFAULT_COMMAND FZF_DEFAULT_OPTS
