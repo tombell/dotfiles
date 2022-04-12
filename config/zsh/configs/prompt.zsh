59f1b8e0171e0a7956c62716542245b0c79c2ba9
Tom Bell <tomb@tomb.io>
2022-04-12 21:56:22 +0100

    Simplify ZSH prompt code

--- config/zsh/configs/prompt.zsh
+++ config/zsh/configs/prompt.zsh
@@ -1,10 +1,10 @@
 git_prompt_info() {
   local current_branch=$(git rev-parse --abbrev-ref HEAD 2> /dev/null)
   if [[ -n $current_branch ]]; then
-    echo "%{$fg[blue]%}⸤$current_branch⸣%{$reset_color%} "
+    echo "%{$fg[blue]%}⸤$current_branch⸣ "
   fi
 }
 
 setopt promptsubst
 
-PS1='%{$fg[green]%}%c %{$reset_color%}$(git_prompt_info)%{$fg[magenta]%}➤ %{$reset_color%}'
+PROMPT='%{$fg[green]%}%c $(git_prompt_info)%{$fg[magenta]%}▶ %{$reset_color%}'
