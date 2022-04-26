6092433a66fc04bee5779fcd2367e069575a3d59
Tom Bell <tomb@tomb.io>
2022-04-26 23:14:51 +0100

    Add $HOME/.cargo/bin to $PATH if exists

--- config/zsh/configs/post/path.zsh
+++ config/zsh/configs/post/path.zsh
@@ -12,8 +12,12 @@ if command -v nodenv >/dev/null; then
   eval "$(nodenv init - --no-rehash)"
 fi
 
-if [[ -d $HOME/.spicetify ]]; then
-  PATH=$HOME/.spicetify:$PATH
+if [[ -d "$HOME/.spicetify" ]]; then
+  PATH="$HOME/.spicetify":$PATH
+fi
+
+if [[ -d "$HOME/.cargo/bin" ]]; then
+  PATH="$HOME/.cargo/bin":$PATH
 fi
 
 export -U GOPATH PATH
