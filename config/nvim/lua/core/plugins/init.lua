262c4d7e676d418208bf8706fa5dfdc107976f48
Tom Bell <tomb@tomb.io>
2022-04-30 11:48:59 +0100

    Remove comments from plugins/init.lua

--- config/nvim/lua/core/plugins/init.lua
+++ config/nvim/lua/core/plugins/init.lua
@@ -1,37 +1,30 @@
 require("packer").startup(function()
-  -- packer
   use "wbthomason/packer.nvim"
 
-  -- colorscheme
   use {
     "catppuccin/nvim",
     as = "catppuccin",
     config = 'require("core.plugins.catppuccin")',
   }
 
-  -- fuzzy finding
   use {
     "nvim-telescope/telescope.nvim",
     requires = { "nvim-lua/plenary.nvim" },
   }
 
-  -- utilities
   use "editorconfig/editorconfig-vim"
   use "tpope/vim-commentary"
 
-  -- highlighting
   use {
     "nvim-treesitter/nvim-treesitter",
     run = ":TSUpdate",
     config = 'require("core.plugins.treesitter")',
   }
 
-  -- lsp
   use {
     "neovim/nvim-lspconfig",
     config = 'require("core.plugins.lsp")',
   }
 
-  -- sql
   use "lifepillar/pgsql.vim"
 end)
