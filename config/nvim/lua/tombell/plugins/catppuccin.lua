95c5c76866dffda0fd7511f2311b72d8f71d5d74
Tom Bell <tomb@tomb.io>
2022-07-19 11:42:03 +0100

    breaking changes in upgrade for catppuccin

--- config/nvim/lua/tombell/plugins/catppuccin.lua
+++ config/nvim/lua/tombell/plugins/catppuccin.lua
@@ -1,28 +1,3 @@
-local catppuccin = require "catppuccin"
-
-catppuccin.setup {
-  integrations = {
-    treesitter = true,
-    native_lsp = {
-      enabled = true,
-      virtual_text = {
-        errors = "italic",
-        hints = "italic",
-        warnings = "italic",
-        information = "italic",
-      },
-      underlines = {
-        errors = "underline",
-        hints = "underline",
-        warnings = "underline",
-        information = "underline",
-      },
-    },
-    telescope = true,
-    markdown = true,
-  },
-}
-
 vim.cmd [[colorscheme catppuccin]]
 
 vim.cmd [[highlight VertSplit guifg=#302D41]]
