a4cd0e36ad09a246a983bc503cd5b1ca9f211162
Tom Bell <tomb@tomb.io>
2022-07-21 09:45:43 +0100

    Set nvim catpuccin flavour to mocha

--- config/nvim/lua/tombell/plugins/catppuccin.lua
+++ config/nvim/lua/tombell/plugins/catppuccin.lua
@@ -1,3 +1,9 @@
+local catpuccin = require "catppuccin"
+
+vim.g.catppuccin_flavour = "mocha"
+
+catpuccin.setup()
+
 vim.cmd [[colorscheme catppuccin]]
 
 vim.cmd [[highlight VertSplit guifg=#302D41]]
