be17851ee2a60490db522f8296028cc83d09a406
Tom Bell <tomb@tomb.io>
2022-09-12 20:39:24 +0100

    Update null_ls.lua

--- config/nvim/lua/tombell/plugins/null_ls.lua
+++ config/nvim/lua/tombell/plugins/null_ls.lua
@@ -4,15 +4,26 @@ local prefer_local = "node_modules/.bin"
 
 null_ls.setup {
   sources = {
-    null_ls.builtins.completion.spell.with { filetypes = { "markdown", "gitcommit" } },
-    null_ls.builtins.formatting.stylua,
+    -- code actions
     null_ls.builtins.code_actions.eslint.with { prefer_local = prefer_local },
+
+    -- completion
+    null_ls.builtins.completion.spell.with { filetypes = { "markdown", "gitcommit" } },
+
+    -- diagnostics
     null_ls.builtins.diagnostics.eslint.with { prefer_local = prefer_local },
+
+    -- formatting
+    -- null_ls.builtins.formatting.goimports,
+    -- null_ls.builtins.formatting.gofmt,
+    -- null_ls.builtins.formatting.prettier,
+    null_ls.builtins.formatting.stylua,
   },
 
   on_attach = function(client, bufnr)
     if client.resolved_capabilities.document_formatting then
       local null_ls_group = vim.api.nvim_create_augroup("null_ls_group", { clear = true })
+
       vim.api.nvim_create_autocmd("BufWritePre", {
         group = null_ls_group,
         command = "lua vim.lsp.buf.formatting_sync()",
