cea6ab4de20eb0ff751812b8df25b4644afe83a7
Tom Bell <tomb@tomb.io>
2022-09-13 20:18:13 +0100

    Update path.zsh ordering

--- config/zsh/configs/post/path.zsh
+++ config/zsh/configs/post/path.zsh
@@ -1,10 +1,10 @@
 GOPATH=$HOME/.go
 
+PATH="$HOME/.dotfiles/bin:$PATH"
+
 if [[ -d "/opt/homebrew/bin" ]]; then
   PATH="/opt/homebrew/bin:$PATH"
 fi
-
-PATH="$HOME/.dotfiles/bin:$PATH"
 
 if command -v rbenv >/dev/null; then
   eval "$(rbenv init - --no-rehash)"
@@ -16,10 +16,6 @@ fi
 
 if [[ -d "$HOME/.cargo/bin" ]]; then
   PATH="$HOME/.cargo/bin":$PATH
-fi
-
-if [[ -d "$HOME/.spicetify" ]]; then
-  PATH="$HOME/.spicetify":$PATH
 fi
 
 export -U GOPATH PATH
