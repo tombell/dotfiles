98f8b2af5b3fd634646f975e45b410f49122363d
Tom Bell <tomb@tomb.io>
2022-09-13 19:36:11 +0100

    Update plugins/catppuccin.lua for overriding highlights

--- config/nvim/lua/tombell/plugins/catppuccin.lua
+++ config/nvim/lua/tombell/plugins/catppuccin.lua
@@ -1,9 +1,21 @@
 local catpuccin = require "catppuccin"
+local mocha = require("catppuccin.palettes").get_palette "mocha"
 
 vim.g.catppuccin_flavour = "mocha"
 
-catpuccin.setup()
+catpuccin.setup {
+  compile = {
+    enabled = true,
+    path = vim.fn.stdpath "cache" .. "/catppuccin",
+  },
+  highlight_overrides = {
+    mocha = {
+      VertSplit = { fg = mocha.surface0 },
+      commentTSDanger = { fg = mocha.red, bg = mocha.base },
+      commentTSNote = { fg = mocha.blue, bg = mocha.base },
+      commentTSWarning = { fg = mocha.yellow, bg = mocha.base },
+    },
+  },
+}
 
 vim.cmd [[colorscheme catppuccin]]
-
-vim.cmd [[highlight VertSplit guifg=#302D41]]
