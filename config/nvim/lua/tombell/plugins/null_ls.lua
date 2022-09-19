83c72e110b078ac027ee93d9488d4cb8540bca94
Tom Bell <tomb@tomb.io>
2022-09-20 00:20:39 +0100

    Update null_ls.lua to add eslint to sources conditionally

--- config/nvim/lua/tombell/plugins/null_ls.lua
+++ config/nvim/lua/tombell/plugins/null_ls.lua
@@ -3,18 +3,17 @@ local null_ls = require "null-ls"
 local prefer_local = "node_modules/.bin"
 
 local sources = {
-  -- code actions
-  null_ls.builtins.code_actions.eslint.with { prefer_local = prefer_local },
-
   -- completion
   null_ls.builtins.completion.spell.with { filetypes = { "markdown", "gitcommit" } },
-
-  -- diagnostics
-  null_ls.builtins.diagnostics.eslint.with { prefer_local = prefer_local },
 
   -- formatting
   null_ls.builtins.formatting.stylua,
 }
+
+if vim.fn.filereadable ".eslintrc.js" == 1 then
+  table.insert(sources, null_ls.builtins.code_actions.eslint.with { prefer_local = prefer_local })
+  table.insert(sources, null_ls.builtins.diagnostics.eslint.with { prefer_local = prefer_local })
+end
 
 if vim.fn.filereadable ".rubocop.yml" == 1 then
   table.insert(sources, null_ls.builtins.diagnostics.rubocop)
