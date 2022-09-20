2acd7fc60ddab9c208034824ca8f0675c8bec9b2
Tom Bell <tomb@tomb.io>
2022-09-20 01:03:36 +0100

    Update lualine mode formatting

--- config/nvim/lua/tombell/plugins/lualine.lua
+++ config/nvim/lua/tombell/plugins/lualine.lua
@@ -7,7 +7,14 @@ lualine.setup {
     section_separators = "",
   },
   sections = {
-    lualine_a = { "mode" },
+    lualine_a = {
+      {
+        "mode",
+        fmt = function(str)
+          return str:sub(1, 1)
+        end,
+      },
+    },
     lualine_b = { "branch", "diff", "diagnostics" },
     lualine_c = { "filename" },
     lualine_x = { "encoding" },
