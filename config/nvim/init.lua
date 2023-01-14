7008a8a4747bd31ea9954006aa83e2b4b5a382d5
Tom Bell <tomb@tomb.io>
2023-01-14 18:07:33 +0000

    Remove vim.g.ruby_path

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -6,8 +6,5 @@ end
 
 vim.g.mapleader = ","
 
--- Ruby files load slow if you don't set this when using rbenv
-vim.g.ruby_path = "$HOME/.rbenv/shims/ruby"
-
 require "tombell.plugins"
 require "tombell.autocmd"
