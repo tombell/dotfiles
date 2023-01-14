0cc38a3d122c998c2014ce7da8c6f2dd4105022b
Tom Bell <tomb@tomb.io>
2023-01-14 16:31:08 +0000

    Remove custom mode format in lualine

--- config/nvim/lua/tombell/plugins/lualine.lua
+++ config/nvim/lua/tombell/plugins/lualine.lua
@@ -2,14 +2,7 @@ local lualine = require "lualine"
 
 lualine.setup {
   sections = {
-    lualine_a = {
-      {
-        "mode",
-        fmt = function(str)
-          return str:sub(1, 1)
-        end,
-      },
-    },
+    lualine_a = { "mode" },
     lualine_b = { "branch", "diff", "diagnostics" },
     lualine_c = { "filename" },
     lualine_x = { "encoding" },
