2d24baa46b4b1035ec10c1494616cc515bb7ed87
Tom Bell <tomb@tomb.io>
2023-01-15 03:39:41 +0000

    Update ident_blankline config

--- config/nvim/lua/tombell/plugins/init.lua
+++ config/nvim/lua/tombell/plugins/init.lua
@@ -15,7 +15,7 @@ packer.startup(function()
   use {
     "lukas-reineke/indent-blankline.nvim",
     config = function()
-      require("indent_blankline").setup()
+      require("indent_blankline").setup { show_current_context = true }
     end,
   }
 
