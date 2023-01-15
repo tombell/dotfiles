dd904ab73da92476aa3020d5d87d210209f60f4f
Tom Bell <tomb@tomb.io>
2023-01-15 23:23:40 +0000

    Add statuscolumn configuration

---
+++ config/nvim/plugin/statuscolumn.lua
@@ -0,0 +1,45 @@
+if statuscolumn then
+  return
+end
+
+statuscolumn = {}
+
+function statuscolumn.linenr()
+  if vim.v.wrap then
+    return ""
+  end
+
+  return vim.v.lnum
+end
+
+function statuscolumn.fold()
+  if vim.v.wrap then
+    return ""
+  end
+
+  local lnum = vim.v.lnum
+
+  if vim.fn.foldlevel(lnum) <= 0 then
+    return "  "
+  end
+
+  if vim.fn.foldlevel(lnum) <= vim.fn.foldlevel(lnum - 1) then
+    return "  "
+  end
+
+  if vim.fn.foldclosed(lnum) == -1 then
+    return " "
+  end
+
+  return " "
+end
+
+vim.opt.statuscolumn = table.concat {
+  "%s",
+  "%=%{v:lua.statuscolumn.linenr()}",
+  " ",
+  "%#FoldColumn#",
+  "%{v:lua.statuscolumn.fold()}",
+  "%#StatusColumnBuffer#",
+  " ",
+}
