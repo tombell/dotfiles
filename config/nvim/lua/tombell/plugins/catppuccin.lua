580f7954bd9109d2010dbbd5083e1b058883ac70
Tom Bell <tomb@tomb.io>
2023-02-15 20:07:20 +0000

    Update catppuccin config

--- config/nvim/lua/tombell/plugins/catppuccin.lua
+++ config/nvim/lua/tombell/plugins/catppuccin.lua
@@ -1,14 +1,14 @@
 local catpuccin = require "catppuccin"
-local mocha = require("catppuccin.palettes").get_palette "mocha"
 
 catpuccin.setup {
-  flavour = "mocha",
   no_italic = true,
   highlight_overrides = {
-    mocha = {
-      Folded = { fg = mocha.blue, bg = mocha.mantle },
-      VertSplit = { fg = mocha.surface0, bg = mocha.base },
-    },
+    mocha = function(mocha)
+      return {
+        Folded = { fg = mocha.blue, bg = mocha.mantle },
+        VertSplit = { fg = mocha.surface0, bg = mocha.base },
+      }
+    end,
   },
 }
 
