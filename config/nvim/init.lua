174330da50a3e51d783dabc8eb8144016aef8fad
Tom Bell <tomb@tomb.io>
2023-02-16 00:06:52 +0000

    Switch from packer.nvim to lazy.nvim

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,8 +1,19 @@
 require "tombell.globals"
 
-if require "tombell.first_load"() then
-  return
+local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system {
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  }
 end
+
+vim.opt.rtp:prepend(lazypath)
 
 vim.g.mapleader = ","
 
--- config/nvim/lua/tombell/plugins/init.lua
+++ config/nvim/lua/tombell/plugins/init.lua
@@ -1,40 +1,40 @@
-local packer = require "packer"
+local lazy = require "lazy"
 
-packer.startup(function()
-  use "wbthomason/packer.nvim"
+lazy.setup {
+  "wbthomason/packer.nvim",
 
-  use "stevearc/dressing.nvim"
+  "stevearc/dressing.nvim",
 
-  use {
+  {
     "lukas-reineke/indent-blankline.nvim",
     config = function()
       require("indent_blankline").setup { show_current_context = true }
     end,
-  }
+  },
 
-  use {
+  {
     "catppuccin/nvim",
-    as = "catppuccin",
+    name = "catppuccin",
     config = function()
       require "tombell.plugins.catppuccin"
     end,
-  }
+  },
 
-  use {
+  {
     "nvim-lualine/lualine.nvim",
     config = function()
       require("lualine").setup()
     end,
-  }
+  },
 
-  use {
+  {
     "lewis6991/gitsigns.nvim",
     config = function()
       require("gitsigns").setup()
     end,
-  }
+  },
 
-  use {
+  {
     "anuvyklack/pretty-fold.nvim",
     config = function()
       require("pretty-fold").setup {
@@ -45,80 +45,80 @@ packer.startup(function()
         fill_char = " ",
       }
     end,
-  }
+  },
 
-  use {
+  {
     "nvim-telescope/telescope.nvim",
     config = function()
       require "tombell.plugins.telescope"
     end,
-    requires = {
+    dependencies = {
       "nvim-lua/plenary.nvim",
     },
-  }
+  },
 
-  use {
+  {
     "numToStr/Comment.nvim",
     config = function()
       require("Comment").setup {
         pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
       }
     end,
-    requires = {
+    dependencies = {
       "JoosepAlviste/nvim-ts-context-commentstring",
     },
-  }
+  },
 
-  use {
+  {
     "rafaelsq/nvim-goc.lua",
     config = function()
       require("nvim-goc").setup()
     end,
-  }
+  },
 
-  use {
+  {
     "nvim-treesitter/nvim-treesitter",
-    run = ":TSUpdate",
+    cmd = "TSUpdate",
     config = function()
       require "tombell.plugins.treesitter"
     end,
-  }
+  },
 
-  use {
+  {
     "neovim/nvim-lspconfig",
     config = function()
       require "tombell.plugins.lsp.servers"
     end,
-  }
+  },
 
-  use {
+  {
     "hrsh7th/nvim-cmp",
     config = function()
       require "tombell.plugins.lsp.cmp"
     end,
-    requires = {
+    dependencies = {
       "L3MON4D3/LuaSnip",
       "hrsh7th/cmp-buffer",
       "hrsh7th/cmp-nvim-lsp",
       "hrsh7th/cmp-nvim-lua",
       "roobert/tailwindcss-colorizer-cmp.nvim",
     },
-  }
+  },
 
-  use {
+  {
     "jose-elias-alvarez/null-ls.nvim",
     config = function()
       require "tombell.plugins.lsp.null-ls"
     end,
-    requires = {
+    dependencies = {
       "nvim-lua/plenary.nvim",
     },
-  }
+  },
 
-  use {
+  {
     "lukas-reineke/virt-column.nvim",
     config = function()
       require("virt-column").setup()
     end,
-  }
-end)
+  },
+}
