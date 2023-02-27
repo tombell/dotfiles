853a226f6b25436ef46b6d7f018ccedd8f353641
Tom Bell <tomb@tomb.io>
2023-02-27 00:05:00 +0000

    wip

--- config/nvim/lua/tombell/plugins/lsp/cmp.lua
+++ config/nvim/lua/tombell/plugins/lsp/cmp.lua
@@ -9,6 +9,9 @@ cmp.setup {
     end
 
     local context = require "cmp.config.context"
+    if vim.api.nvim_get_mode().mode == "c" then
+      return true
+    end
     return not context.in_treesitter_capture "comment" and not context.in_syntax_group "Comment"
   end,
   snippet = {
