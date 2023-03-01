9d6646ad1a5fb30f360351486b2f55f3b29cd85f
Tom Bell <tomb@tomb.io>
2023-03-01 15:32:36 +0000

    Only add rubocop if .rubocop.yml file exists

--- config/nvim/lua/tombell/plugins/lsp/null-ls.lua
+++ config/nvim/lua/tombell/plugins/lsp/null-ls.lua
@@ -2,15 +2,20 @@ local U = require "tombell.plugins.lsp.utils"
 
 local null_ls = require "null-ls"
 
-null_ls.setup {
-  sources = {
-    null_ls.builtins.formatting.prettierd.with {
-      filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
-    },
-    null_ls.builtins.formatting.gofmt,
-    null_ls.builtins.formatting.stylua,
-    null_ls.builtins.formatting.goimports,
-    null_ls.builtins.diagnostics.rubocop,
+local sources = {
+  null_ls.builtins.formatting.gofmt,
+  null_ls.builtins.formatting.goimports,
+  null_ls.builtins.formatting.prettierd.with {
+    filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
   },
+  null_ls.builtins.formatting.stylua,
+}
+
+if vim.fn.filereadable ".rubocop.yml" == 1 then
+  table.insert(sources, null_ls.builtins.diagnostics.rubocop)
+end
+
+null_ls.setup {
+  sources = sources,
   on_attach = U.fmt_on_save,
 }
