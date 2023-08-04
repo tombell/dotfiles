ed4d368acc2ff305a3c28c9188a4e891461658d2
Tom Bell <tomb@tomb.io>
2023-08-05 00:59:03 +0100

    Formatting

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,5 +1,10 @@
 if vim.fn.exists "g:vscode" == 1 then
-  vim.keymap.set({ "n", "o", "x" }, "gc", "<cmd>call VSCodeCall('editor.action.commentLine')<cr><esc>", { remap = true })
+  vim.keymap.set(
+    { "n", "o", "x" },
+    "gc",
+    "<cmd>call VSCodeCall('editor.action.commentLine')<cr><esc>",
+    { remap = true }
+  )
   vim.keymap.set("n", "gcc", "<cmd>call VSCodeCall('editor.action.commentLine')<cr><esc>", { remap = true })
   return
 end
