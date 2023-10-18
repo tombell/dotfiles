aae5a6e976cf23c9137969a40d5488b2e406c7e8
Tom Bell <tomb@tomb.io>
2023-10-18 13:23:59 +0100

    Update neovim formatting configuration to not fallback to LSP formatting

--- config/nvim/lua/tombell/plugins/formatting.lua
+++ config/nvim/lua/tombell/plugins/formatting.lua
@@ -3,16 +3,16 @@ return {
     "stevearc/conform.nvim",
     opts = {
       formatters_by_ft = {
-        lua = { "stylua" },
+        go = { "goimports", "gofmt" },
         javascript = { "prettier" },
         javascriptreact = { "prettier" },
+        lua = { "stylua" },
         typescript = { "prettier" },
         typescriptreact = { "prettier" },
-        go = { "goimports", "gofmt" },
       },
       format_on_save = {
         timeout_ms = 500,
-        lsp_fallback = true,
+        lsp_fallback = false,
       },
     },
   },
