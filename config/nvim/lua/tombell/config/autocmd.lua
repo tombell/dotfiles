2004d652ea78ba366bbe4ff791929c5001ae784e
Tom Bell <tomb@tomb.io>
2023-10-19 14:23:12 +0100

    Update autocmds and add ftplugins as autocmds

--- config/nvim/lua/tombell/config/autocmd.lua
+++ config/nvim/lua/tombell/config/autocmd.lua
@@ -1,19 +1,76 @@
+local function augroup(name)
+  return vim.api.nvim_create_augroup("tombell_" .. name, { clear = true })
+end
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+  group = augroup "highlight_yank",
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
 vim.api.nvim_create_autocmd("FileType", {
-  group = vim.api.nvim_create_augroup("spell_group", { clear = true }),
+  group = augroup "close_with_q",
+  pattern = {
+    "checkhealth",
+    "help",
+    "lspinfo",
+    "neotest-output",
+    "neotest-output-panel",
+    "neotest-summary",
+    "qf",
+    "query",
+    "startuptime",
+  },
+  callback = function(event)
+    vim.bo[event.buf].buflisted = false
+    vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = event.buf, silent = true })
+  end,
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+  group = augroup "wrap_spell",
   pattern = { "markdown", "gitcomit" },
-  command = "setlocal spell",
+  callback = function()
+    vim.opt_local.wrap = true
+    vim.opt_local.spell = true
+  end,
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+  group = augroup "go",
+  pattern = { "go" },
+  callback = function()
+    vim.bo.tabstop = 4
+    vim.bo.shiftwidth = 4
+    vim.bo.softtabstop = 4
+    vim.bo.expandtab = false
+  end,
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+  group = augroup "gitcommit",
+  pattern = { "gitcommit" },
+  callback = function()
+    vim.bo.textwidth = 72
+  end,
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+  group = augroup "make",
+  pattern = { "make" },
+  callback = function()
+    vim.bo.tabstop = 4
+    vim.bo.shiftwidth = 4
+    vim.bo.softtabstop = 4
+    vim.bo.expandtab = false
+  end,
 })
 
 vim.api.nvim_create_autocmd("BufEnter", {
-  group = vim.api.nvim_create_augroup("dotenv_group", { clear = true }),
-  pattern = {
-    ".env",
-    ".env.development",
-    ".env.e2e",
-    ".env.local",
-    ".env.production",
-    ".env.staging",
-    ".env.test",
-  },
-  command = "setlocal ft=sh",
+  group = augroup "dotenv",
+  pattern = { ".env", ".env.*" },
+  callback = function()
+    vim.opt_local.filetype = "sh"
+  end,
 })
