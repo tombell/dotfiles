071be2213d445455a321efdf66fa5f6129e698be
Tom Bell <tomb@tomb.io>
2023-10-20 20:00:34 +0100

    Update telescope opts

--- config/nvim/lua/tombell/plugins/editor.lua
+++ config/nvim/lua/tombell/plugins/editor.lua
@@ -14,9 +14,29 @@ return {
 
       return {
         defaults = {
+          prompt_prefix = " ",
+          selection_caret = " ",
+          get_selection_window = function()
+            local wins = vim.api.nvim_list_wins()
+            table.insert(wins, 1, vim.api.nvim_get_current_win())
+
+            for _, win in ipairs(wins) do
+              local buf = vim.api.nvim_win_get_buf(win)
+
+              if vim.bo[buf].buftype == "" then
+                return win
+              end
+            end
+
+            return 0
+          end,
           mappings = {
             i = {
-              ["<esc>"] = actions.close,
+              ["<C-Down>"] = actions.cycle_history_next,
+              ["<C-Up>"] = actions.cycle_history_prev,
+            },
+            n = {
+              ["q"] = actions.close,
             },
           },
         },
