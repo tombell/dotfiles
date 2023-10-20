56ff6cb2ee0a4274e5092ccffe580c21def34ffd
Tom Bell <tomb@tomb.io>
2023-10-20 19:50:34 +0100

    Add folke/which-key.nvim plugin

--- config/nvim/lua/tombell/plugins/editor.lua
+++ config/nvim/lua/tombell/plugins/editor.lua
@@ -30,6 +30,39 @@ return {
   },
 
   {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    opts = {
+      icons = {
+        separator = "",
+      },
+      plugins = { spelling = true },
+      defaults = {
+        mode = { "n", "v" },
+        ["g"] = { name = "+goto" },
+        ["]"] = { name = "+next" },
+        ["["] = { name = "+prev" },
+        ["<leader>b"] = { name = "+buffer" },
+        ["<leader>c"] = { name = "+code" },
+        ["<leader>f"] = { name = "+file/find" },
+        ["<leader>g"] = { name = "+git" },
+        ["<leader>gh"] = { name = "+hunks" },
+        ["<leader>q"] = { name = "+quit/session" },
+        ["<leader>s"] = { name = "+search" },
+        ["<leader>t"] = { name = "+test" },
+        ["<leader>u"] = { name = "+ui" },
+        ["<leader>w"] = { name = "+windows" },
+        ["<leader>x"] = { name = "+diagnostics/quickfix" },
+      },
+    },
+    config = function(_, opts)
+      local wk = require "which-key"
+      wk.setup(opts)
+      wk.register(opts.defaults)
+    end,
+  },
+
+  {
     "lewis6991/gitsigns.nvim",
     opts = {},
   },
