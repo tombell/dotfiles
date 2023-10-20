3043e676faa170cb7890aa5bad47fb2b66068d37
Tom Bell <tomb@tomb.io>
2023-10-20 19:47:55 +0100

    Add lua_ls to lsp servers table

--- config/nvim/lua/tombell/plugins/lsp/init.lua
+++ config/nvim/lua/tombell/plugins/lsp/init.lua
@@ -7,12 +7,25 @@ return {
       local capabilities = vim.lsp.protocol.make_client_capabilities()
       capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 
-      for _, server in ipairs { "gopls", "solargraph", "sourcekit", "tsserver" } do
+      local servers = {
+        "gopls",
+        "lua_ls",
+        "solargraph",
+        "sourcekit",
+        "tsserver",
+      }
+
+      for _, server in ipairs(servers) do
         lspconfig[server].setup {
           capabilities = capabilities,
-          on_attach = function(client, buf)
+          on_attach = function(client, buffer)
             require("tombell.plugins.lsp.keymaps").on_attach(client, buffer)
           end,
+          settings = {
+            Lua = {
+              diagnostics = { globals = { "vim" } },
+            },
+          },
         }
       end
     end,
