a544f0d5bd024fce9f097004ea6f09456bd3e515
Tom Bell <tomb@tomb.io>
2023-10-20 20:25:11 +0100

    Add telescope util

---
+++ config/nvim/lua/tombell/util/telescope.lua
@@ -0,0 +1,33 @@
+local util = require "tombell.util"
+
+local M = setmetatable({}, {
+  __call = function(m, ...)
+    return m.telescope(...)
+  end,
+})
+
+function M.telescope(builtin, opts)
+  local params = { builtin = builtin, opts = opts }
+
+  return function()
+    builtin = params.builtin
+    opts = vim.tbl_deep_extend("force", { cwd = util.root() }, opts or {})
+
+    if builtin == "files" then
+      if vim.loop.fs_stat((opts.cwd or vim.loop.cwd()) .. "/.git") then
+        opts.show_untracked = true
+        builtin = "git_files"
+      else
+        builtin = "find_files"
+      end
+    end
+
+    require("telescope.builtin")[builtin](opts)
+  end
+end
+
+function M.config_files()
+  return M.telescope("find_files", { cwd = vim.fn.stdpath "config" })
+end
+
+return M
