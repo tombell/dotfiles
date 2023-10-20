c120a72899251d3ae37d65017d04657aff234a57
Tom Bell <tomb@tomb.io>
2023-10-20 22:54:00 +0100

    Add expand_tabs and trim util functions

--- config/nvim/lua/tombell/util/init.lua
+++ config/nvim/lua/tombell/util/init.lua
@@ -5,4 +5,42 @@ local M = setmetatable({}, {
   end,
 })
 
+function M.expand_tabs(str, ts)
+  local new = str:sub(1, 0)
+  local pad = " "
+  local ti = 0
+  local i = 0
+
+  while true do
+    i = str:find("\t", i, true)
+
+    if not i then
+      if ti == 0 then
+        new = str
+      else
+        new = new .. str:sub(ti + 1)
+      end
+
+      break
+    end
+
+    if ti + 1 == i then
+      new = new .. pad:rep(ts)
+    else
+      local append = str:sub(ti + 1, i - 1)
+
+      new = new .. append .. pad:rep(ts - vim.api.nvim_strwidth(append) % ts)
+    end
+
+    ti = i
+    i = i + 1
+  end
+
+  return new
+end
+
+function M.trim(str)
+  return str:gsub("^%s*(.-)%s*$", "%1")
+end
+
 return M
