a9f15eedad53ba3aaa14b1b8a4b120434a3ec788
Tom Bell <tomb@tomb.io>
2023-10-25 23:43:52 +0100

    Remove formatters from conform configuration

--- config/nvim/lua/tombell/plugins/formatting.lua
+++ config/nvim/lua/tombell/plugins/formatting.lua
@@ -1,14 +1,10 @@
 return {
   {
     "stevearc/conform.nvim",
+    dependencies = { "mason.nvim" },
     opts = {
       formatters_by_ft = {
-        go = { "goimports", "gofmt" },
-        javascript = { "prettier" },
-        javascriptreact = { "prettier" },
         lua = { "stylua" },
-        typescript = { "prettier" },
-        typescriptreact = { "prettier" },
       },
       format_on_save = {
         timeout_ms = 500,
