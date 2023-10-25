73145600da95b31cdc33238f24ee284622f95c68
Tom Bell <tomb@tomb.io>
2023-10-25 23:48:30 +0100

    Update neovim LSP configuration to use mason

--- config/nvim/lua/tombell/plugins/lsp/init.lua
+++ config/nvim/lua/tombell/plugins/lsp/init.lua
@@ -1,32 +1,112 @@
 return {
   {
     "neovim/nvim-lspconfig",
-    config = function()
-      local lspconfig = require "lspconfig"
-
-      local capabilities = vim.lsp.protocol.make_client_capabilities()
-      capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
-
-      local servers = {
-        "gopls",
-        "lua_ls",
-        "solargraph",
-        "sourcekit",
-        "tsserver",
-      }
-
-      for _, server in ipairs(servers) do
-        lspconfig[server].setup {
-          capabilities = capabilities,
-          on_attach = function(client, buffer)
-            require("tombell.plugins.lsp.keymaps").on_attach(client, buffer)
-          end,
+    event = { "BufReadPost", "BufNewFile", "BufWritePre", "VeryLazy" },
+    dependencies = {
+      "mason.nvim",
+      "williamboman/mason-lspconfig.nvim",
+    },
+    opts = {
+      servers = {
+        lua_ls = {
           settings = {
             Lua = {
               diagnostics = { globals = { "vim" } },
             },
           },
-        }
+        },
+      },
+      setup = {},
+    },
+    config = function(_, opts)
+      local servers = opts.servers
+      local has_cmp, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
+      local capabilities = vim.tbl_deep_extend(
+        "force",
+        {},
+        vim.lsp.protocol.make_client_capabilities(),
+        has_cmp and cmp_nvim_lsp.default_capabilities() or {},
+        opts.capabilities or {}
+      )
+
+      local function setup(server)
+        local server_opts = vim.tbl_deep_extend("force", {
+          capabilities = vim.deepcopy(capabilities),
+        }, servers[server] or {})
+
+        if opts.setup[server] then
+          if opts.setup[server](server, server_opts) then
+            return
+          end
+        elseif opts.setup["*"] then
+          if opts.setup["*"](server, server_opts) then
+            return
+          end
+        end
+
+        require("lspconfig")[server].setup(server_opts)
+      end
+
+      local have_mason, mlsp = pcall(require, "mason-lspconfig")
+      local all_mslp_servers = {}
+
+      if have_mason then
+        all_mslp_servers = vim.tbl_keys(require("mason-lspconfig.mappings.server").lspconfig_to_package)
+      end
+
+      local ensure_installed = {}
+
+      for server, server_opts in pairs(servers) do
+        if server_opts then
+          server_opts = server_opts == true and {} or server_opts
+
+          if server_opts.mason == false or not vim.tbl_contains(all_mslp_servers, server) then
+            setup(server)
+          else
+            ensure_installed[#ensure_installed + 1] = server
+          end
+        end
+      end
+
+      if have_mason then
+        mlsp.setup { ensure_installed = ensure_installed, handlers = { setup } }
+      end
+    end,
+  },
+
+  {
+    "williamboman/mason.nvim",
+    cmd = "Mason",
+    keys = { { "<leader>cm", "<cmd>Mason<cr>", desc = "Mason" } },
+    build = ":MasonUpdate",
+    opts = {
+      ensure_installed = {
+        "stylua",
+      },
+    },
+    config = function(_, opts)
+      require("mason").setup(opts)
+      local mr = require "mason-registry"
+      mr:on("package:install:success", function()
+        vim.defer_fn(function()
+          require("lazy.core.handler.event").trigger {
+            event = "FileType",
+            buf = vim.api.nvim_get_current_buf(),
+          }
+        end, 100)
+      end)
+      local function ensure_installed()
+        for _, tool in ipairs(opts.ensure_installed) do
+          local p = mr.get_package(tool)
+          if not p:is_installed() then
+            p:install()
+          end
+        end
+      end
+      if mr.refresh then
+        mr.refresh(ensure_installed)
+      else
+        ensure_installed()
       end
     end,
   },
