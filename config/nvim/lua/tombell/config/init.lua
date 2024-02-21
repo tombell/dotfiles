d5db8a94b4ba1c7e0b0793cf9102e7d3af831008
Tom Bell <tomb@tomb.io>
2024-02-21 01:32:12 +0000

    Add svelte import to config/init.lua

--- config/nvim/lua/tombell/config/init.lua
+++ config/nvim/lua/tombell/config/init.lua
@@ -8,9 +8,10 @@ function M.setup()
   require("lazy").setup({
     { import = "tombell.plugins" },
     { import = "tombell.plugins.lang.go" },
-    { import = "tombell.plugins.lang.ruby" },
-    { import = "tombell.plugins.lang.typescript" },
     { import = "tombell.plugins.lang.javascript" },
+    { import = "tombell.plugins.lang.ruby" },
+    { import = "tombell.plugins.lang.svelte" },
+    { import = "tombell.plugins.lang.typescript" },
   }, {
     install = {
       colorscheme = { "catppuccin" },
