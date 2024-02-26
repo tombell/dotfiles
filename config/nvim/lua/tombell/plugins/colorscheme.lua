41524cec0e17cac4e3fd214b06cd711306a2b57f
Tom Bell <tomb@tomb.io>
2024-02-26 00:02:37 +0000

    Remove references to old colorscheme

--- config/nvim/lua/tombell/plugins/colorscheme.lua
+++ config/nvim/lua/tombell/plugins/colorscheme.lua
@@ -3,21 +3,4 @@ return {
     "rose-pine/neovim",
     name = "rose-pine",
   },
-
-  {
-    "catppuccin/nvim",
-    lazy = true,
-    name = "catppuccin",
-    opts = {
-      flavour = "frappe",
-      highlight_overrides = {
-        frappe = function(frappe)
-          return {
-            Folded = { fg = frappe.blue, bg = frappe.mantle },
-            VertSplit = { fg = frappe.surface0, bg = frappe.base },
-          }
-        end,
-      },
-    },
-  },
 }
