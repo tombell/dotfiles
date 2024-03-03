78d9c99e668eb293fa08eac7abaaa5aea0c67189
Tom Bell <tomb@tomb.io>
2024-03-03 01:34:23 +0000

    Remove rbenv and nodenv from zsh config

--- config/zsh/configs/post/path.zsh
+++ config/zsh/configs/post/path.zsh
@@ -3,11 +3,3 @@ export PROJECTS="$HOME/Code"
 export GOPATH=$HOME/.local/share/go
 
 export PATH="$HOME/.dotfiles/bin:$GOPATH/bin:$PATH"
-
-if command -v rbenv >/dev/null; then
-  eval "$(rbenv init - --no-rehash)"
-fi
-
-if command -v nodenv >/dev/null; then
-  eval "$(nodenv init - --no-rehash)"
-fi
