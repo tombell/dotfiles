f7caca3824c89744171056950e651ee505fd5e08
Tom Bell <tomb@tomb.io>
2024-06-11 11:49:55 +0100

    Update zsh path configuration

--- config/zsh/configs/post/path.zsh
+++ config/zsh/configs/post/path.zsh
@@ -2,4 +2,11 @@ export PROJECTS="$HOME/Code"
 
 export GOPATH=$HOME/.local/share/go
 
-export PATH="$HOME/.dotfiles/bin:$GOPATH/bin:$PATH"
+# dotfile binaries/scripts
+export PATH="$HOME/.dotfiles/bin:$PATH"
+
+# golang binaries
+export PATH="$GOPATH/bin:$PATH"
+
+# mise shims
+export PATH="$HOME/.local/share/mise/shims:$PATH"
