e1095c782d1832dc5a6bc7f47824802e10b4a81b
Tom Bell <tomb@tomb.io>
2024-06-15 15:36:07 +0100

    Update stark config to adjust for window gaps

--- config/stark/stark.js
+++ config/stark/stark.js
@@ -8,14 +8,20 @@ const ctrlShiftOpt = ["ctrl", "shift", "opt"];
 
 const margin = 12;
 
-const gridColumns = 12;
-const gridRoes = 10;
-
 // -----------------------------------------------------------------------------
 // WINDOW MANAGEMENT
 // -----------------------------------------------------------------------------
 
-const grid = (columns, rows, x, y, addMargin = true) => {
+const grid = (
+  totalCols,
+  totalRows,
+  columns,
+  rows,
+  x,
+  y,
+  addMargin = true,
+  adjustMargin = false,
+) => {
   const win = Window.focused();
 
   if (!win) {
@@ -29,78 +35,89 @@ const grid = (columns, rows, x, y, addMargin = true) => {
     y: offsetY,
   } = win.screen.flippedVisibleFrame;
 
-  const cellWidth = width / gridColumns;
-  const cellHeight = height / gridRoes;
+  const cellWidth = width / totalCols;
+  const cellHeight = height / totalRows;
 
   const marginValue = addMargin ? margin : 0;
 
-  win.setFrame({
+  const frame = {
     width: cellWidth * columns - marginValue * 2,
     height: cellHeight * rows - marginValue * 2,
     x: offsetX + marginValue + cellWidth * x,
     y: offsetY + marginValue + cellHeight * y,
-  });
+  };
+
+  if (adjustMargin) {
+    frame.width += margin / 2;
+    frame.x -= x === 0 ? 0 : margin / 2;
+  }
+
+  win.setFrame(frame);
 };
 
 // Centre full
-Keymap.on("c", ctrlShift, () => grid(12, 10, 0, 0));
+Keymap.on("c", ctrlShift, () => grid(12, 10, 12, 10, 0, 0));
 
 // Centre medium
-Keymap.on("x", ctrlShift, () => grid(10, 8, 1, 1, false));
+Keymap.on("x", ctrlShift, () => grid(12, 10, 10, 8, 1, 1, false));
 
 // Centre medium, full height
-Keymap.on("x", ctrlShiftOpt, () => grid(8, 10, 2, 0));
+Keymap.on("x", ctrlShiftOpt, () => grid(12, 10, 8, 10, 2, 0));
 
 // Centre small
-Keymap.on("z", ctrlShift, () => grid(8, 6, 2, 2, false));
+Keymap.on("z", ctrlShift, () => grid(12, 10, 8, 6, 2, 2, false));
 
 // Centre small, full height
-Keymap.on("z", ctrlShiftOpt, () => grid(6, 10, 3, 0));
+Keymap.on("z", ctrlShiftOpt, () => grid(12, 10, 6, 10, 3, 0));
 
 // Left-half
-Keymap.on("h", ctrlShift, () => grid(6, 10, 0, 0));
+Keymap.on("h", ctrlShift, () => grid(12, 10, 6, 10, 0, 0, true, true));
 
 // Right-half
-Keymap.on("l", ctrlShift, () => grid(6, 10, 6, 0));
+Keymap.on("l", ctrlShift, () => grid(12, 10, 6, 10, 6, 0, true, true));
 
 // Left-one third
-Keymap.on("h", ctrlOpt, () => grid(4, 10, 0, 0));
+Keymap.on("h", ctrlOpt, () => grid(12, 10, 4, 10, 0, 0, true, true));
 
 // Right-one third
-Keymap.on("l", ctrlOpt, () => grid(4, 10, 8, 0));
+Keymap.on("l", ctrlOpt, () => grid(12, 10, 4, 10, 8, 0, true, true));
 
 // Left-two thirds
-Keymap.on("h", ctrlShiftOpt, () => grid(8, 10, 0, 0));
+Keymap.on("h", ctrlShiftOpt, () => grid(12, 10, 8, 10, 0, 0, true, true));
 
 // Right-two thirds
-Keymap.on("l", ctrlShiftOpt, () => grid(8, 10, 4, 0));
+Keymap.on("l", ctrlShiftOpt, () => grid(12, 10, 8, 10, 4, 0, true, true));
 
 // Move window to previous display
 Keymap.on("left", ctrlShift, () => {
   const win = Window.focused();
 
-  if (!win || win.screen.id === win.screen.previous.id) {
-    return;
+  if (win && win.screen.id !== win.screen.previous.id) {
+    const { x, y } = win.frame;
+    const frame = win.screen.previous.flippedVisibleFrame;
+
+    win.setFrame({
+      ...win.frame,
+      x: x - frame.x,
+      y: y - frame.y,
+    });
   }
-
-  const { x, y } = win.frame;
-  const frame = win.screen.previous.flippedVisibleFrame;
-
-  win.setFrame({ ...win.frame, x: x - frame.x, y: y - frame.y });
 });
 
 // Move window to next display
 Keymap.on("right", ctrlShift, () => {
   const win = Window.focused();
 
-  if (!win || win.screen.id === win.screen.next.id) {
-    return;
+  if (win && win.screen.id !== win.screen.next.id) {
+    const { x, y } = win.frame;
+    const frame = win.screen.next.flippedVisibleFrame;
+
+    win.setFrame({
+      ...win.frame,
+      x: x + frame.x,
+      y: y + frame.y,
+    });
   }
-
-  const { x, y } = win.frame;
-  const frame = win.screen.next.flippedVisibleFrame;
-
-  win.setFrame({ ...win.frame, x: x + frame.x, y: y + frame.y });
 });
 
 // -----------------------------------------------------------------------------
