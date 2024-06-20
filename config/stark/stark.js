64a3e0a99389cbf1d52d383ddcff966e81f34203
Tom Bell <tomb@tomb.io>
2024-06-20 23:09:46 +0100

    Update stark config to simplify grid code

--- config/stark/stark.js
+++ config/stark/stark.js
@@ -6,87 +6,91 @@ const ctrlShift = ["ctrl", "shift"];
 const ctrlOpt = ["ctrl", "opt"];
 const ctrlShiftOpt = ["ctrl", "shift", "opt"];
 
-const margin = 12;
+const padding = 12;
+const gap = 12;
 
 // -----------------------------------------------------------------------------
 // WINDOW MANAGEMENT
 // -----------------------------------------------------------------------------
 
-const grid = (
-  totalCols,
-  totalRows,
-  columns,
-  rows,
-  x,
-  y,
-  addMargin = true,
-  adjustMargin = false,
-) => {
+const grid = (c, r, w, h, x, y) => {
   const win = Window.focused();
 
   if (!win) {
     return;
   }
 
-  const {
-    width,
-    height,
-    x: offsetX,
-    y: offsetY,
-  } = win.screen.flippedVisibleFrame;
+  if (x >= c) x = c - 1;
+  if (y >= r) y = r - 1;
 
-  const cellWidth = width / totalCols;
-  const cellHeight = height / totalRows;
+  if (w <= 0) w = 1;
+  if (h <= 0) h = 1;
 
-  const marginValue = addMargin ? margin : 0;
+  if (w > c - x) w = c - x;
+  if (h > r - y) h = r - y;
 
-  const frame = {
-    width: cellWidth * columns - marginValue * 2,
-    height: cellHeight * rows - marginValue * 2,
-    x: offsetX + marginValue + cellWidth * x,
-    y: offsetY + marginValue + cellHeight * y,
-  };
+  const bounds = win.screen.flippedVisibleFrame;
 
-  if (adjustMargin) {
-    frame.width += margin / 2;
-    frame.x -= x === 0 ? 0 : margin / 2;
+  bounds.x += padding;
+  bounds.width -= padding * 2;
+  bounds.y += padding;
+  bounds.height -= padding * 2;
+
+  if (x > 0) {
+    bounds.x += gap;
+    bounds.width -= gap;
   }
 
-  win.setFrame(frame);
+  if (y > 0) {
+    bounds.y += gap;
+    bounds.height -= gap;
+  }
+
+  if (c > x + w) bounds.width -= gap;
+  if (r > y + h) bounds.height -= gap;
+
+  const cw = bounds.width / c;
+  const ch = bounds.height / r;
+  const fx = bounds.x + bounds.width - cw * (c - x);
+  const fy = bounds.y + bounds.height - ch * (r - y);
+  const fw = cw * w;
+  const fh = ch * h;
+
+  win.setFrame({ x: fx, y: fy, width: fw, height: fh });
 };
 
 // Centre full
 Keymap.on("c", ctrlShift, () => grid(12, 10, 12, 10, 0, 0));
 
 // Centre medium
-Keymap.on("x", ctrlShift, () => grid(12, 10, 10, 8, 1, 1, false));
+Keymap.on("x", ctrlShift, () => grid(12, 10, 10, 8, 1, 1));
 
 // Centre medium, full height
 Keymap.on("x", ctrlShiftOpt, () => grid(12, 10, 8, 10, 2, 0));
 
 // Centre small
-Keymap.on("z", ctrlShift, () => grid(12, 10, 8, 6, 2, 2, false));
+Keymap.on("z", ctrlShift, () => grid(12, 10, 8, 6, 2, 2));
 
 // Centre small, full height
 Keymap.on("z", ctrlShiftOpt, () => grid(12, 10, 6, 10, 3, 0));
 
 // Left-half
-Keymap.on("h", ctrlShift, () => grid(12, 10, 6, 10, 0, 0, true, true));
+Keymap.on("h", ctrlShift, () => grid(12, 10, 6, 10, 0, 0));
 
 // Right-half
-Keymap.on("l", ctrlShift, () => grid(12, 10, 6, 10, 6, 0, true, true));
+Keymap.on("l", ctrlShift, () => grid(12, 10, 6, 10, 6, 0));
 
 // Left-one third
-Keymap.on("h", ctrlOpt, () => grid(12, 10, 4, 10, 0, 0, true, true));
+Keymap.on("h", ctrlOpt, () => grid(12, 10, 4, 10, 0, 0));
 
 // Right-one third
-Keymap.on("l", ctrlOpt, () => grid(12, 10, 4, 10, 8, 0, true, true));
+Keymap.on("l", ctrlOpt, () => grid(12, 10, 4, 10, 8, 0));
 
 // Left-two thirds
-Keymap.on("h", ctrlShiftOpt, () => grid(12, 10, 8, 10, 0, 0, true, true));
+Keymap.on("h", ctrlShiftOpt, () => grid(12, 10, 8, 10, 0, 0));
 
 // Right-two thirds
-Keymap.on("l", ctrlShiftOpt, () => grid(12, 10, 8, 10, 4, 0, true, true));
+Keymap.on("l", ctrlShiftOpt, () => grid(12, 10, 8, 10, 4, 0));
 
 // Move window to previous display
 Keymap.on("left", ctrlShift, () => {
