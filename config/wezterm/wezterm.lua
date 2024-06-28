7f9e7fe5ce291505b58ff634b08e3ece080b3c5a
Tom Bell <tomb@tomb.io>
2024-06-28 15:56:53 +0100

    Update wezterm configuration

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -3,11 +3,11 @@ local wezterm = require "wezterm"
 local config = wezterm.config_builder()
 
 config.font = wezterm.font("Iosevka NFM", { weight = "Bold", italic = false })
-config.font_size = 12.5
+config.font_size = 12
 config.color_scheme = "Catppuccin Mocha"
 config.enable_tab_bar = false
 config.window_decorations = "RESIZE"
-config.window_padding = { left = 30, right = 30, top = 30, bottom = 30 }
+config.window_padding = { left = 26, right = 26, top = 26, bottom = 26 }
 config.window_close_confirmation = "NeverPrompt"
 
 config.keys = {
