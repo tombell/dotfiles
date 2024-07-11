bc464818c3a14f9e45383af93f3619ebea5278ea
Tom Bell <tomb@tomb.io>
2024-07-11 22:43:45 +0100

    Update wezterm configuration to handle high and low DPI screens

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -1,5 +1,25 @@
 local wezterm = require "wezterm"
 
+local function recompute_padding(window)
+  local window_dims = window:get_dimensions()
+  local factor = 144 / window_dims.dpi
+  local padding = 36 / factor
+
+  local overrides = window:get_config_overrides() or {}
+
+  overrides.window_padding = { left = padding, right = padding, top = padding, bottom = padding }
+
+  window:set_config_overrides(overrides)
+end
+
+wezterm.on("window-resized", function(window)
+  recompute_padding(window)
+end)
+
+wezterm.on("window-config-reloaded", function(window)
+  recompute_padding(window)
+end)
+
 local config = wezterm.config_builder()
 
 config.font = wezterm.font("Iosevka NFM", { weight = "Bold", italic = false })
@@ -7,7 +27,6 @@ config.font_size = 12
 config.color_scheme = "Catppuccin Mocha"
 config.enable_tab_bar = false
 config.window_decorations = "RESIZE"
-config.window_padding = { left = 26, right = 26, top = 26, bottom = 26 }
 config.window_close_confirmation = "NeverPrompt"
 
 config.keys = {
