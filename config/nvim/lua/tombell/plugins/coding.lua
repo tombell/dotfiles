2d17883661a3ae417429b4129e21b20a0e11cc91
Tom Bell <tomb@tomb.io>
2024-07-13 14:46:29 +0100

    Update to use folke/ts-comments.nvim

--- config/nvim/lua/tombell/plugins/coding.lua
+++ config/nvim/lua/tombell/plugins/coding.lua
@@ -49,22 +49,8 @@ return {
   },
 
   {
-    "JoosepAlviste/nvim-ts-context-commentstring",
-    lazy = true,
-    opts = {
-      enable_autocmd = false,
-    },
-  },
-
-  {
-    "echasnovski/mini.comment",
+    "folke/ts-comments.nvim",
     event = "VeryLazy",
-    opts = {
-      options = {
-        custom_commentstring = function()
-          return require("ts_context_commentstring.internal").calculate_commentstring() or vim.bo.commentstring
-        end,
-      },
-    },
+    opts = {},
   },
 }
