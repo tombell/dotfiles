c3e2c6162f50eeac9836fded63c3ad472572eab8
Tom Bell <tomb@tomb.io>
2024-07-13 02:56:09 +0100

    Add biome to js/ts formatting

--- config/nvim/lua/tombell/plugins/lang/javascript-typescript.lua
+++ config/nvim/lua/tombell/plugins/lang/javascript-typescript.lua
@@ -25,22 +25,47 @@ return {
     "williamboman/mason.nvim",
     opts = function(_, opts)
       opts.ensure_installed = opts.ensure_installed or {}
-      vim.list_extend(opts.ensure_installed, { "prettierd" })
+      vim.list_extend(opts.ensure_installed, { "biome", "prettierd" })
     end,
   },
 
   {
     "stevearc/conform.nvim",
     optional = true,
-    opts = {
-      formatters_by_ft = {
-        javascript = { "prettierd" },
-        javascriptreact = { "prettierd" },
-        json = { "prettierd" },
-        typescript = { "prettierd" },
-        typescriptreact = { "prettierd" },
-      },
-    },
+    opts = function(_, opts)
+      local function add_formatters(tbl)
+        for ft, formatters in pairs(tbl) do
+          if opts.formatters_by_ft[ft] == nil then
+            opts.formatters_by_ft[ft] = formatters
+          else
+            vim.list_extend(opts.formatters_by_ft[ft], formatters)
+          end
+        end
+      end
+
+      add_formatters {
+        javascript = { "biome", "prettierd" },
+        javascriptreact = { "biome", "prettierd" },
+        json = { "biome", "prettierd" },
+        typescript = { "biome", "prettierd" },
+        typescriptreact = { "biome", "prettierd" },
+      }
+
+      opts.formatters = {
+        biome = {
+          condition = function(_, ctx)
+            return vim.fs.find({ "biome.json" }, { path = ctx.filename, upward = true })[1]
+          end,
+        },
+        prettierd = {
+          condition = function(_, ctx)
+            return not vim.fs.find({ "biome.json" }, { path = ctx.filename, upward = true })[1]
+          end,
+        },
+      }
+
+      return opts
+    end,
   },
 
   {
