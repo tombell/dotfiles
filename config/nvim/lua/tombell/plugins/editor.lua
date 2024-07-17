cbff8ffd2aebe6ce09672f057176666d3c0e7167
Tom Bell <tomb@tomb.io>
2024-07-17 19:59:12 +0100

    Remove neo-tree opts not required

--- config/nvim/lua/tombell/plugins/editor.lua
+++ config/nvim/lua/tombell/plugins/editor.lua
@@ -31,16 +31,7 @@ return {
       },
       default_component_configs = {
         indent = {
-          with_expanders = true, -- if nil and file nesting is enabled, will enable expanders
-          expander_collapsed = "",
-          expander_expanded = "",
-          expander_highlight = "NeoTreeExpander",
-        },
-        git_status = {
-          symbols = {
-            unstaged = "󰄱",
-            staged = "󰱒",
-          },
+          with_expanders = true,
         },
       },
     },
