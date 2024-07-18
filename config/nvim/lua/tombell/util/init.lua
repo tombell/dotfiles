449c238dfd21e7a34c9a4446d209d6e3c7573f2b
Tom Bell <tomb@tomb.io>
2024-07-18 01:16:41 +0100

    Simplify util/ui code

--- config/nvim/lua/tombell/util/init.lua
+++ config/nvim/lua/tombell/util/init.lua
@@ -7,8 +7,12 @@ setmetatable(M, {
   end,
 })
 
+function M.get_plugin(name)
+  return require("lazy.core.config").spec.plugins[name]
+end
+
 function M.opts(name)
-  local plugin = require("lazy.core.config").spec.plugins[name]
+  local plugin = M.get_plugin(name)
 
   if not plugin then
     return {}
@@ -16,40 +20,6 @@ function M.opts(name)
 
   local Plugin = require "lazy.core.plugin"
   return Plugin.values(plugin, "opts", false)
-end
-
-function M.expand_tabs(str, ts)
-  local new = str:sub(1, 0)
-  local pad = " "
-  local ti = 0
-  local i = 0
-
-  while true do
-    i = str:find("\t", i, true)
-
-    if not i then
-      if ti == 0 then
-        new = str
-      else
-        new = new .. str:sub(ti + 1)
-      end
-
-      break
-    end
-
-    if ti + 1 == i then
-      new = new .. pad:rep(ts)
-    else
-      local append = str:sub(ti + 1, i - 1)
-
-      new = new .. append .. pad:rep(ts - vim.api.nvim_strwidth(append) % ts)
-    end
-
-    ti = i
-    i = i + 1
-  end
-
-  return new
 end
 
 return M
--- config/nvim/lua/tombell/util/ui.lua
+++ config/nvim/lua/tombell/util/ui.lua
@@ -1,19 +1,46 @@
 local M = {}
 
-M.exclude_foldend_filetypes = {
-  "markdown",
-  "python",
-  "skbdrc",
-  "tex",
-}
+function M.expand_tabs(str, ts)
+  local new = str:sub(1, 0)
+  local pad = " "
+  local ti = 0
+  local i = 0
+
+  while true do
+    i = str:find("\t", i, true)
+
+    if not i then
+      if ti == 0 then
+        new = str
+      else
+        new = new .. str:sub(ti + 1)
+      end
+
+      break
+    end
+
+    if ti + 1 == i then
+      new = new .. pad:rep(ts)
+    else
+      local append = str:sub(ti + 1, i - 1)
+
+      new = new .. append .. pad:rep(ts - vim.api.nvim_strwidth(append) % ts)
+    end
+
+    ti = i
+    i = i + 1
+  end
+
+  return new
+end
 
 function M.foldtext()
   local ret = {
-    tombell.expand_tabs(vim.api.nvim_buf_get_lines(0, vim.v.foldstart - 1, vim.v.foldstart, true)[1], vim.bo.tabstop),
+    M.expand_tabs(vim.api.nvim_buf_get_lines(0, vim.v.foldstart - 1, vim.v.foldstart, true)[1], vim.bo.tabstop),
     "󰇘",
   }
 
-  if not vim.tbl_contains(M.exclude_foldend_filetypes, vim.bo.filetype) then
+  if not vim.tbl_contains(tombell.config.ui.foldend.exclude, vim.bo.filetype) then
     local line = vim.api.nvim_buf_get_lines(0, vim.v.foldend - 1, vim.v.foldend, true)[1]:gsub("^%s*(.-)%s*$", "%1")
     table.insert(ret, line)
   end
@@ -21,42 +48,20 @@ function M.foldtext()
   return table.concat(ret, " ")
 end
 
-function M.linenr()
-  local win = vim.g.statusline_winid
-  local is_num = vim.wo[win].number
-  local is_relnum = vim.wo[win].relativenumber
-
-  local lnum = ""
-
-  if (is_num or is_relnum) and vim.v.virtnum == 0 then
-    if vim.v.relnum == 0 then
-      lnum = is_num and "%l" or "%r"
-    else
-      lnum = is_relnum and "%r" or "%l"
-    end
-
-    lnum = "%=" .. lnum .. " "
-  end
-
-  return lnum
-end
-
 function M.fold()
   if vim.fn.foldlevel(vim.v.lnum) > vim.fn.foldlevel(vim.v.lnum - 1) then
     if vim.fn.foldclosed(vim.v.lnum) == -1 then
       return "%#FoldColumn#"
-    else
-      return "%#FoldColumn#"
     end
-  else
-    return " "
+
+    return "%#FoldColumn#"
   end
+
+  return " "
 end
 
 function M.statuscolumn()
-  local foldcolumn = M.fold()
-  local lnum = vim.v.lnum
-  return string.format("%d %s ", lnum, foldcolumn)
+  return string.format("%d %s ", vim.v.lnum, M.fold())
 end
 
 return M
