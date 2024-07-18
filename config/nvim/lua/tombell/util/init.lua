a0b19279fb31d3323f4c35153bef21453b54e79b
Tom Bell <tomb@tomb.io>
2024-07-18 01:18:31 +0100

    Add is_loaded and on_load functions to util/init.lua

--- config/nvim/lua/tombell/util/init.lua
+++ config/nvim/lua/tombell/util/init.lua
@@ -22,4 +22,25 @@ function M.opts(name)
   return Plugin.values(plugin, "opts", false)
 end
 
+function M.is_loaded(name)
+  local Config = require "lazy.core.config"
+  return Config.plugins[name] and Config.plugins[name]._.loaded
+end
+
+function M.on_load(name, fn)
+  if M.is_loaded(name) then
+    fn(name)
+  else
+    vim.api.nvim_create_autocmd("User", {
+      pattern = "LazyLoad",
+      callback = function(event)
+        if event.data == name then
+          fn(name)
+          return true
+        end
+      end,
+    })
+  end
+end
+
 return M
