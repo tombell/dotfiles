ec0e2f9ffa6ab475c34d5b137f23df963cac2c95
Tom Bell <tomb@tomb.io>
2024-09-01 17:04:35 +0100

    Update font for bigger nerd font icons

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -23,7 +23,7 @@ end)
 local config = wezterm.config_builder()
 
 config.audible_bell = "Disabled"
-config.font = wezterm.font("Iosevka NFM", { weight = "Bold" })
+config.font = wezterm.font("Iosevka NFP", { weight = "Bold" })
 config.font_size = 12
 config.color_scheme = "Catppuccin Mocha"
 config.enable_tab_bar = false
