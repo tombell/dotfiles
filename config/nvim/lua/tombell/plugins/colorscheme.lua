1e7a45f63a1f4356168197172b5549f3834fba0e
Tom Bell <tomb@tombell.dev>
2024-12-21 23:21:40 +0000

    Add tokyonight highlight overrides

--- config/nvim/lua/tombell/plugins/colorscheme.lua
+++ config/nvim/lua/tombell/plugins/colorscheme.lua
@@ -20,9 +20,6 @@ return {
         blink_cmp = true,
       },
     },
-    -- init = function()
-    --   vim.cmd.colorscheme "catppuccin"
-    -- end,
   },
 
   {
@@ -32,5 +29,11 @@ return {
     init = function()
       vim.cmd.colorscheme "tokyonight-night"
     end,
+    opts = {
+      on_highlights = function(highlights, colors)
+        highlights.Folded = { fg = colors.blue0, bg = colors.bg_float }
+        highlights.WinSeparator = { fg = colors.blue7, bg = colors.bg }
+      end,
+    },
   },
 }
