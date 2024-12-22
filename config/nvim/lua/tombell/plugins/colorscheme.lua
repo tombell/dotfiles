1fbbcc9ac4557aead99f04a5cca8310250a09c43
Tom Bell <tomb@tombell.dev>
2024-12-22 00:53:19 +0000

    Update ordering of colorscheme plugins

--- config/nvim/lua/tombell/plugins/colorscheme.lua
+++ config/nvim/lua/tombell/plugins/colorscheme.lua
@@ -3,14 +3,14 @@ return {
     "folke/tokyonight.nvim",
     lazy = false,
     priority = 1000,
-    init = function()
-      vim.cmd.colorscheme "tokyonight-night"
-    end,
     opts = {
       on_highlights = function(highlights, colors)
         highlights.Folded = { fg = colors.blue0, bg = colors.bg_float }
         highlights.WinSeparator = { fg = colors.blue7, bg = colors.bg }
       end,
     },
+    init = function()
+      vim.cmd.colorscheme "tokyonight-night"
+    end,
   },
 }
