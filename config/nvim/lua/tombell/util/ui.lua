dc915d1a35552e76d9f986adf69c65edd17ff08e
Tom Bell <tomb@tombell.dev>
2024-12-22 01:52:07 +0000

    Update foldtext helper method

--- config/nvim/lua/tombell/util/ui.lua
+++ config/nvim/lua/tombell/util/ui.lua
@@ -1,5 +1,13 @@
 local M = {}
 
+local foldend_exclude = {
+  "markdown",
+  "python",
+  "skbdrc",
+  "tex",
+  "yaml",
+}
+
 function M.expand_tabs(str, ts)
   local new = str:sub(1, 0)
   local pad = " "
@@ -40,7 +48,7 @@ function M.foldtext()
     "󰇘",
   }
 
-  if not vim.tbl_contains(tombell.config.ui.foldend.exclude, vim.bo.filetype) then
+  if not vim.tbl_contains(foldend_exclude, vim.bo.filetype) then
     local line = vim.api.nvim_buf_get_lines(0, vim.v.foldend - 1, vim.v.foldend, true)[1]:gsub("^%s*(.-)%s*$", "%1")
     table.insert(ret, line)
   end
