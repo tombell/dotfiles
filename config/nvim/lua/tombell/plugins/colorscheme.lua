2e37fec44a540838aa49d0d9287c2bbd3abd939d
Tom Bell <tomb@tombell.dev>
2024-12-22 00:38:08 +0000

    Remove catppuccin/nvim

--- config/nvim/lua/tombell/plugins/colorscheme.lua
+++ config/nvim/lua/tombell/plugins/colorscheme.lua
@@ -1,28 +1,5 @@
 return {
   {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    lazy = true,
-    priority = 1000,
-    opts = {
-      flavour = "mocha",
-      highlight_overrides = {
-        mocha = function(mocha)
-          return {
-            FloatBorder = { fg = mocha.blue, bg = mocha.none },
-            Folded = { fg = mocha.blue, bg = mocha.mantle },
-            NormalFloat = { fg = mocha.text, bg = mocha.none },
-            WinSeparator = { fg = mocha.surface0, bg = mocha.base },
-          }
-        end,
-      },
-      integrations = {
-        blink_cmp = true,
-      },
-    },
-  },
-
-  {
     "folke/tokyonight.nvim",
     lazy = false,
     priority = 1000,
