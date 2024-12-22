5327c215cc50a6379b3ad81df38d3f10ef22c36a
Tom Bell <tomb@tombell.dev>
2024-12-22 01:52:47 +0000

    Refactor tombell/config/init.lua into tombell/init.lua

--- config/nvim/lua/tombell/init.lua
+++ config/nvim/lua/tombell/init.lua
@@ -1,7 +1,26 @@
+_G.tombell = require "tombell.util"
+
 local M = {}
 
 function M.setup()
-  require("tombell.config").setup()
+  require "tombell.config.options"
+  require "tombell.config.autocmds"
+  require "tombell.config.keymaps"
+
+  require("lazy").setup({
+    { import = "tombell.plugins" },
+  }, {
+    install = {
+      colorscheme = { "tokyonight-night" },
+    },
+    ui = {
+      backdrop = 100,
+      border = "rounded",
+    },
+    change_detection = {
+      notify = false,
+    },
+  })
 end
 
 return M
