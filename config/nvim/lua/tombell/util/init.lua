82e374959441ae646652f1cee4926fe1379bed7e
Tom Bell <tomb@tombell.dev>
2024-12-27 12:44:37 +0000

    Remove unused methods

--- config/nvim/lua/tombell/util/init.lua
+++ config/nvim/lua/tombell/util/init.lua
@@ -7,19 +7,4 @@ setmetatable(M, {
   end,
 })
 
-function M.get_plugin(name)
-  return require("lazy.core.config").spec.plugins[name]
-end
-
-function M.opts(name)
-  local plugin = M.get_plugin(name)
-
-  if not plugin then
-    return {}
-  end
-
-  local Plugin = require "lazy.core.plugin"
-  return Plugin.values(plugin, "opts", false)
-end
-
 return M
