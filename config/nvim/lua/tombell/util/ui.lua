881605d2e256da5c960febba98bd01697621e0eb
Tom Bell <tomb@tombell.dev>
2025-01-06 23:52:25 +0000

    Update to use fold method from pleat.nvim

--- config/nvim/lua/tombell/util/ui.lua
+++ config/nvim/lua/tombell/util/ui.lua
@@ -1,23 +1,11 @@
 local M = {}
 
-function M.fold()
-  return vim.api.nvim_win_call(vim.g.statusline_winid, function()
-    if vim.fn.foldclosed(vim.v.lnum) >= 0 then
-      return vim.opt.fillchars:get().foldclose
-    elseif tostring(vim.treesitter.foldexpr(vim.v.lnum)):sub(1, 1) == ">" then
-      return vim.opt.fillchars:get().foldopen
-    else
-      return " "
-    end
-  end)
-end
-
 function M.statuscolumn()
   return table.concat {
     "%l",
     " ",
     "%s",
-    M.fold(),
+    require("pleat").fold(),
     "  ",
   }
 end
