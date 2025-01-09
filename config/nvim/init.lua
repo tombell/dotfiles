31024e3cc056f1b53088e88afef681e7d73589ec
Tom Bell <tomb@tombell.dev>
2025-01-09 15:15:24 +0000

    Add disable of missing-fields in init.lua

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -19,6 +19,7 @@ vim.opt.rtp:prepend(lazypath)
 if vim.env.PROF then
   local snacks = vim.fn.stdpath "data" .. "/lazy/snacks.nvim"
   vim.opt.rtp:append(snacks)
+  ---@diagnostic disable: missing-fields
   require("snacks.profiler").startup {
     startup = { event = "VimEnter" },
   }
