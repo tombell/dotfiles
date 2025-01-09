21632a9bc18b8fa550b49a34479ea67c9d9fc816
Tom Bell <tomb@tombell.dev>
2025-01-09 16:17:47 +0000

    Improve LSP loading event

--- config/nvim/lua/tombell/plugins/lsp.lua
+++ config/nvim/lua/tombell/plugins/lsp.lua
@@ -7,7 +7,7 @@ return {
       "williamboman/mason-lspconfig.nvim",
       "WhoIsSethDaniel/mason-tool-installer.nvim",
     },
-    event = "VeryLazy",
+    event = { "BufReadPost", "BufNewFile", "BufWritePre" },
     config = function()
       vim.diagnostic.config {
         underline = true,
