5d84ccc5b946ecc441d9e188a459181e81801088
Tom Bell <tomb@tombell.dev>
2025-02-06 01:06:37 +0000

    wip: remove old telescope keymaps for trouble

--- config/nvim/lua/plugins/editor.lua
+++ config/nvim/lua/plugins/editor.lua
@@ -118,9 +118,6 @@ return {
       { "[t", function() require("todo-comments").jump_prev() end, desc = "Previous Todo Comment" },
       { "<leader>xt", "<cmd>Trouble todo toggle<cr>", desc = "Todo (Trouble)" },
       { "<leader>xT", "<cmd>Trouble todo toggle filter = {tag = {TODO,FIX,FIXME}}<cr>", desc = "Todo/Fix/Fixme (Trouble)" },
-      { "<leader>st", "<cmd>TodoTelescope<cr>", desc = "Todo" },
-      { "<leader>sT", "<cmd>TodoTelescope keywords=TODO,FIX,FIXME<cr>", desc = "Todo/Fix/Fixme" },
-
       ---@diagnostic disable-next-line: undefined-field
       { "<leader>st", function() Snacks.picker.todo_comments() end, desc = "Todo" },
       ---@diagnostic disable-next-line: undefined-field
