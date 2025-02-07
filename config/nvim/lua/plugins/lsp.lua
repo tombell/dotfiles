493e144d40351ba2058e79d8247b05b928de095c
Tom Bell <tomb@tombell.dev>
2025-02-07 00:12:30 +0000

    Update nvim lsp configuration

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -3,25 +3,24 @@ return {
   {
     "neovim/nvim-lspconfig",
     dependencies = {
-      "williamboman/mason.nvim",
       "williamboman/mason-lspconfig.nvim",
-      "WhoIsSethDaniel/mason-tool-installer.nvim",
     },
     event = { "BufReadPost", "BufNewFile" },
     opts = {
       servers = {
         gopls = {},
         lua_ls = {},
+        ruby_lsp = { enabled = false },
         solargraph = {
-          mason = false,
           enabled = vim.fs.find(".solargraph.yml", { path = vim.uv.cwd(), upward = true })[1] ~= nil,
-          cmd = { "asdf", "exec", "solargraph", "stdio" },
         },
+        tailwindcss = {},
         ts_ls = {
           init_options = {
             preferences = { importModuleSpecifierPreference = "non-relative" },
           },
         },
+        vtsls = { enabled = false },
         zls = {},
       },
     },
@@ -80,26 +79,47 @@ return {
         end
       end
 
-      vim.list_extend(ensure_installed, {
-        "biome",
-        "gofumpt",
-        "goimports",
-        "prettier",
-        "stylua",
-      })
-
-      require("mason").setup()
-      require("mason-tool-installer").setup { ensure_installed = ensure_installed }
-      ---@diagnostic disable: missing-fields
-      require("mason-lspconfig").setup { handlers = { setup } }
+      require("mason-lspconfig").setup {
+        automatic_installation = true,
+        ensure_installed = ensure_installed,
+        handlers = { setup },
+      }
     end,
   },
 
   -- mason.nvim
   {
     "williamboman/mason.nvim",
+    cmd = "Mason",
     keys = {
       { "<leader>cm", "<cmd>Mason<cr>", desc = "Mason" },
     },
+    opts_extend = { "ensure_installed" },
+    opts = {
+      ensure_installed = {},
+    },
+    config = function(_, opts)
+      require("mason").setup(opts)
+
+      local mr = require "mason-registry"
+
+      mr:on("package:install:success", function()
+        vim.defer_fn(function()
+          require("lazy.core.handler.event").trigger {
+            event = "FileType",
+            buf = vim.api.nvim_get_current_buf(),
+          }
+        end, 100)
+      end)
+
+      mr.refresh(function()
+        for _, tool in ipairs(opts.ensure_installed) do
+          local p = mr.get_package(tool)
+          if not p:is_installed() then
+            p:install()
+          end
+        end
+      end)
+    end,
   },
 }
