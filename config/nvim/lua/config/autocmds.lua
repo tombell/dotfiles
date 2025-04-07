7e48979bfb2b86c4bd46b5daf5a40aaf1d7cd27f
Tom Bell <tomb@tombell.dev>
2025-04-07 10:47:22 +0100

    Update vim function name

--- config/nvim/lua/config/autocmds.lua
+++ config/nvim/lua/config/autocmds.lua
@@ -8,7 +8,7 @@ vim.api.nvim_create_autocmd("BufEnter", {
 vim.api.nvim_create_autocmd("TextYankPost", {
   group = vim.api.nvim_create_augroup("tombell-highlight-yaml", { clear = true }),
   callback = function()
-    vim.highlight.on_yank()
+    vim.hl.on_yank()
   end,
 })
 
