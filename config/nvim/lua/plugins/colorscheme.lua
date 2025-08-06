f6e000bd2c5dc353e955ed9e1ef772568fe79aa1
Tom Bell <tomb@tombell.dev>
2025-08-06 23:46:43 +0100

    wip: vague

--- config/nvim/lua/plugins/colorscheme.lua
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -22,10 +22,10 @@ return {
     lazy = false,
     priority = 1000,
     opts = {
-      on_highlights = function(highlights, colors)
-        -- highlights.Folded = { fg = colors.blue0 }
-        -- highlights.WinSeparator = { fg = colors.bg_visual }
-        -- highlights.SnacksIndentScope = { fg = colors.blue0 }
+      on_highlights = function(highlights)
+        highlights.Folded = { bg = "#252530", fg = "#8ba9c1" }
+        highlights.WinSeparator = { fg = "#1c1c24" }
+        highlights.SnacksIndentScope = { fg = "#c9b1ca" }
       end,
     },
     init = function()
