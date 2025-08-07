0a27366bac0a3ceac2b1663fc68dca0c03101328
Tom Bell <tomb@tombell.dev>
2025-08-08 00:02:51 +0100

    Remove diagnostic disable comment

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,7 +1,6 @@
 if vim.env.PROF then
   local snacks = vim.fn.stdpath "data" .. "/lazy/snacks.nvim"
   vim.opt.rtp:append(snacks)
-  ---@diagnostic disable: missing-fields
   require("snacks.profiler").startup {
     startup = { event = "VimEnter" },
   }
