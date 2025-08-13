4e0a721ffe70553f0e9f35fe473fbc9a4fcd22cd
Tom Bell <tomb@tombell.dev>
2025-08-13 11:05:51 +0100

    Update snacks.picker keymaps

--- config/nvim/lua/plugins/editor.lua
+++ config/nvim/lua/plugins/editor.lua
@@ -71,10 +71,11 @@ return {
       { "<leader>fb", function() Snacks.picker.buffers() end, desc = "Buffers" },
       { "<leader>fc", function() Snacks.picker.files({ cwd = vim.fn.stdpath "config" --[[@as string]] }) end, desc = "Find Config File" },
       { "<leader>ff", function() Snacks.picker.files({ cwd = util.root() }) end, desc = "Find Files (Root Dir)" },
-      { "<leader>fF", function() Snacks.picker.files() end, desc = "Find Files (cwd)" },
+      { "<leader>fF", function() Snacks.picker.files() end, desc = "Find Files" },
       { "<leader>fg", function() Snacks.picker.git_files() end, desc = "Find Files (Git)" },
       { "<leader>fr", function() Snacks.picker.recent() end, desc = "Recent" },
-      { "<leader>fR", function() Snacks.picker.recent({ filter = { cwd = true } }) end, desc = "Recent (cwd)" },
+      { "<leader>fR", function() Snacks.picker.recent({ filter = { cwd = true } }) end, desc = "Recent (Current Dir)" },
+      { "<leader>fp", function() Snacks.picker.projects() end, desc = "Projects" },
 
       -- Git
       { "<leader>gc", function() Snacks.picker.git_log() end, desc = "Git Log" },
@@ -82,20 +83,23 @@ return {
 
       -- Grep
       { "<leader>sb", function() Snacks.picker.lines() end, desc = "Buffer Lines" },
-      { "<leader>sB", function() Snacks.picker.grep_buffers() end, desc = "Grep Open Buffers" },
+      { "<leader>sB", function() Snacks.picker.grep_buffers() end, desc = "Grep (Buffers)" },
       { "<leader>sg", function() Snacks.picker.grep({ cwd = util.root() }) end, desc = "Grep (Root Dir)" },
-      { "<leader>sG", function() Snacks.picker.grep() end, desc = "Grep (cwd)" },
-      { "<leader>sw", function() Snacks.picker.grep_word({ cwd = util.root() }) end, desc = "Visual selection or word (cwd)", mode = { "n", "x" } },
-      { "<leader>sW", function() Snacks.picker.grep_word() end, desc = "Visual selection or word (cwd)", mode = { "n", "x" } },
+      { "<leader>sG", function() Snacks.picker.grep() end, desc = "Grep" },
+      { "<leader>sw", function() Snacks.picker.grep_word({ cwd = util.root() }) end, desc = "Visual selection or word (Root Dir)", mode = { "n", "x" } },
+      { "<leader>sW", function() Snacks.picker.grep_word() end, desc = "Visual selection or word", mode = { "n", "x" } },
 
       -- Search
       { '<leader>s"', function() Snacks.picker.registers() end, desc = "Registers" },
+      { '<leader>s/', function() Snacks.picker.search_history() end, desc = "Search History" },
       { "<leader>sa", function() Snacks.picker.autocmds() end, desc = "Autocmds" },
       { "<leader>sc", function() Snacks.picker.command_history() end, desc = "Command History" },
       { "<leader>sC", function() Snacks.picker.commands() end, desc = "Commands" },
       { "<leader>sd", function() Snacks.picker.diagnostics() end, desc = "Diagnostics" },
+      { "<leader>sD", function() Snacks.picker.diagnostics_buffer() end, desc = "Buffer Diagnostics" },
       { "<leader>sh", function() Snacks.picker.help() end, desc = "Help Pages" },
       { "<leader>sH", function() Snacks.picker.highlights() end, desc = "Highlights" },
+      { "<leader>si", function() Snacks.picker.icons() end, desc = "Icons" },
       { "<leader>sj", function() Snacks.picker.jumps() end, desc = "Jumps" },
       { "<leader>sk", function() Snacks.picker.keymaps() end, desc = "Keymaps" },
       { "<leader>sl", function() Snacks.picker.loclist() end, desc = "Location List" },
@@ -103,7 +107,7 @@ return {
       { "<leader>sm", function() Snacks.picker.marks() end, desc = "Marks" },
       { "<leader>sR", function() Snacks.picker.resume() end, desc = "Resume" },
       { "<leader>sq", function() Snacks.picker.qflist() end, desc = "Quickfix List" },
-      { "<leader>qp", function() Snacks.picker.projects() end, desc = "Projects" },
+      { "<leader>su", function() Snacks.picker.undo() end, desc = "Undotree" },
 
       -- UI
       { "<leader>uC", function() Snacks.picker.colorschemes() end, desc = "Colorschemes" },
