8947d4b1fdb2629143e52f116d9c47d10a2a1e7a
Tom Bell <tomb@tombell.dev>
2025-08-21 12:09:53 +0100

    I hate nvim-treesitter rewrite

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -2,6 +2,7 @@ return {
   -- nvim-treesitter
   {
     "nvim-treesitter/nvim-treesitter",
+    commit = "42fc28",
     dependencies = {
       { "starkwm/tree-sitter-skbdrc", config = true },
     },
