cd377f29935a0b2dda4a3fb210efd6a45341cfa9
Tom Bell <tomb@tombell.dev>
2025-08-30 21:56:26 +0100

    Update rpi treesitter overrides

--- tag-rpi/config/nvim/lua/plugins/treesitter.lua
+++ tag-rpi/config/nvim/lua/plugins/treesitter.lua
@@ -1,47 +1,7 @@
-local ensure_installed = {
-  "bash",
-  "diff",
-  "git_config",
-  "git_rebase",
-  "ini",
-  "json",
-  "lua",
-  "luadoc",
-  "make",
-  "python",
-  "query",
-  "sql",
-  "ssh_config",
-  "tmux",
-  "toml",
-  "udev",
-  "vimdoc",
-  "yaml",
-}
-
-vim.api.nvim_create_autocmd("User", {
-  pattern = "TSUpdate",
-  callback = function()
-    ---@diagnostic disable: missing-fields
-    require("nvim-treesitter.parsers").skbdrc = {
-      install_info = {
-        url = "https://github.com/starkwm/tree-sitter-skbdrc",
-        queries = "queries/skbdrc",
-      },
-    }
-  end,
-})
-
 vim.api.nvim_create_autocmd("FileType", {
   pattern = "*",
-  callback = function(ev)
-    local ok = pcall(function()
-      vim.treesitter.get_parser(ev.buf, vim.treesitter.language.get_lang(vim.bo.filetype))
-    end)
-
-    if ok then
-      vim.treesitter.start()
-    end
+  callback = function()
+    pcall(vim.treesitter.start)
   end,
 })
 
@@ -50,11 +10,8 @@ return {
   {
     "nvim-treesitter/nvim-treesitter",
     branch = "main",
-    config = function()
-      require("nvim-treesitter").setup {
-        install_dir = vim.fn.stdpath "data" .. "/site",
-      }
-      require("nvim-treesitter").install(ensure_installed)
-    end,
+    opts = {
+      install_dir = vim.fn.stdpath "data" .. "/site",
+    },
   },
 }
