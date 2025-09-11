02ba2252832396b2a3312c7fdfed939081190d4c
Tom Bell <tomb@tombell.dev>
2025-09-11 22:00:46 +0100

    Add reading SSH keys from 1password

--- scripts/linux.sh
+++ scripts/linux.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+"$(dirname "$0")/ssh.sh"
+
 source "$(dirname "$0")/common.sh"
 
 rcup_install linux
--- scripts/macos.sh
+++ scripts/macos.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+"$(dirname "$0")/ssh.sh"
+
 source "$(dirname "$0")/common.sh"
 
 "$(dirname "$0")/brew.sh"
