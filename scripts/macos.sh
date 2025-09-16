f38825e235bf3785614c94437bdf5fb1fe13f59f
Tom Bell <tomb@tombell.dev>
2025-09-16 18:33:02 +0100

    Add scripts/mise.sh to install tools automatically

--- scripts/macos.sh
+++ scripts/macos.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-"$(dirname "$0")/ssh.sh"
-
 source "$(dirname "$0")/common.sh"
 
+"$(dirname "$0")/ssh.sh"
 "$(dirname "$0")/brew.sh"
-
 rcup_install macos -- -S karabiner
-
-"$(dirname "$0")/defaults.sh"
+# "$(dirname "$0")/defaults.sh"
+"$(dirname "$0")/mise.sh"
