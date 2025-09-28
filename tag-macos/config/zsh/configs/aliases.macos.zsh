640c0aed48d5cf34873607e9a7d8f2a5481baa0c
Tom Bell <tomb@tombell.dev>
2025-09-28 20:34:30 +0100

    Update brew aliases

--- tag-macos/config/zsh/configs/aliases.macos.zsh
+++ tag-macos/config/zsh/configs/aliases.macos.zsh
@@ -2,8 +2,7 @@ alias ds='find . -name ".DS_Store" -type f -delete'
 
 alias safe='xattr -r -d com.apple.quarantine'
 
-alias bup="brew upgrade && brew cleanup -s --prune=all"
-
 alias bl="brew list -1 --formula"
 alias blc="brew list -1 --cask"
 alias bd="brew leaves | xargs brew deps --formula --for-each | sed \"s/^.*:/$(tput setaf 4)&$(tput sgr0)/\""
+alias bup="brew update && brew upgrade && brew cleanup -s --prune=all"
