46583cc0d40b6287531321f7b9954fb381ca49de
Tom Bell <tomb@tombell.dev>
2025-10-10 17:36:55 +0100

    Update linux plugin zsh

--- tag-linux/config/zsh/configs/plugins.zsh
+++ tag-linux/config/zsh/configs/plugins.zsh
@@ -1,5 +1,6 @@
 plugins=(
   "/usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh"
+  "/usr/share/zsh/plugins/fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh"
 )
 
 for plugin in "${plugins[@]}"; do
