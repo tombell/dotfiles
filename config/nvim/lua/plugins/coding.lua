7b4c8943a0c6992d7db0025f9960d19a7d5bf727
Tom Bell <tomb@tombell.dev>
2025-10-10 20:19:40 +0100

    wip: vim.pack.add

--- config/nvim/lua/plugins/coding.lua
+++ config/nvim/lua/plugins/coding.lua
@@ -1,50 +1,33 @@
-return {
-  -- blink.cmp
-  {
-    "saghen/blink.cmp",
-    version = "v1.*",
-    event = "InsertEnter",
-    opts = {
-      keymap = {
-        preset = "enter",
-        ["<tab>"] = { "select_next", "fallback" },
-        ["<s-tab>"] = { "select_prev", "fallback" },
-      },
-      signature = { enabled = false },
-      completion = {
-        list = {
-          selection = { preselect = false, auto_insert = true },
-        },
-      },
-      sources = {
-        default = { "lazydev", "lsp", "path", "snippets", "buffer" },
-        providers = {
-          lazydev = {
-            name = "LazyDev",
-            module = "lazydev.integrations.blink",
-            score_offset = 100,
-          },
-        },
-      },
+require("blink.cmp").setup {
+  keymap = {
+    preset = "enter",
+    ["<tab>"] = { "select_next", "fallback" },
+    ["<s-tab>"] = { "select_prev", "fallback" },
+  },
+  signature = { enabled = false },
+  completion = {
+    ghost_text = { enabled = false },
+    list = {
+      selection = { auto_insert = false },
     },
   },
-
-  -- lazydev.nvim
-  {
-    "folke/lazydev.nvim",
-    ft = "lua",
-    opts = {
-      library = {
-        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
-        { path = "snacks.nvim", words = { "Snacks" } },
+  sources = {
+    default = { "lazydev", "lsp", "path", "snippets", "buffer" },
+    providers = {
+      lazydev = {
+        name = "LazyDev",
+        module = "lazydev.integrations.blink",
+        score_offset = 100,
       },
     },
-  },
-
-  -- ts-comments.nvim
-  {
-    "folke/ts-comments.nvim",
-    event = { "BufReadPost", "BufNewFile", "BufWritePre" },
-    opts = {},
   },
 }
+
+require("lazydev").setup {
+  library = {
+    { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+    { path = "snacks.nvim", words = { "Snacks" } },
+  },
+}
+
+require("ts-comments").setup()
