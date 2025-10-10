6d6488ab37d4a32c40ce04b645f0cb453bcee3f1
Tom Bell <tomb@tombell.dev>
2025-10-10 20:19:40 +0100

    wip: vim.pack.add

--- config/nvim/after/plugin/lsp.lua
+++ config/nvim/after/plugin/lsp.lua
@@ -1,22 +1,20 @@
-vim.lsp.set_log_level "OFF"
+vim.lsp.log.set_level "OFF"
 
-if vim.fn.has "nvim-0.11" == 1 then
-  vim.lsp.config("vtsls", {
-    settings = {
-      typescript = { preferences = { importModuleSpecifier = "non-relative" } },
-      javascript = { preferences = { importModuleSpecifier = "non-relative" } },
+vim.lsp.config("vtsls", {
+  settings = {
+    typescript = { preferences = { importModuleSpecifier = "non-relative" } },
+    javascript = { preferences = { importModuleSpecifier = "non-relative" } },
+  },
+})
+
+vim.lsp.config("gopls", {
+  settings = {
+    codelenses = {
+      test = true,
+      tidy = true,
     },
-  })
+  },
+})
 
-  vim.lsp.config("gopls", {
-    settings = {
-      codelenses = {
-        test = true,
-        tidy = true,
-      },
-    },
-  })
-
-  vim.lsp.enable("solargraph", vim.fs.find(".solargraph.yml", { path = vim.uv.cwd(), upward = true })[1] ~= nil)
-  vim.lsp.enable { "gopls", "lua_ls", "sourcekit", "vtsls", "zls" }
-end
+vim.lsp.enable("solargraph", vim.fs.find(".solargraph.yml", { path = vim.uv.cwd(), upward = true })[1] ~= nil)
+vim.lsp.enable { "gopls", "lua_ls", "sourcekit", "vtsls", "zls" }
