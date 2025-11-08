401d6ba83f02fc4da64e80da6cfc521200327279
Tom Bell <tomb@tombell.dev>
2025-11-08 03:19:18 +0000

    Remove old aliases

--- config/zsh/configs/aliases.zsh
+++ config/zsh/configs/aliases.zsh
@@ -6,8 +6,5 @@ alias e="nvim"
 
 alias path='echo $PATH | tr -s ":" "\n"'
 
-alias d="cd $HOME/.dotfiles"
-alias n="cd $HOME/.dotfiles/config/nvim"
-
 alias urepo='for d in *; do (cd $d && pwd && git pull && echo); done'
 alias brepo='for d in *; do (cd "$d" && pwd && git branch && echo); done'
