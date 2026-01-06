2873227a1b877198972090d8e8d363f938125d39
Tom Bell <tomb@tombell.dev>
2026-01-06 14:08:53 +0000

    Update neotest-jest plugin

--- config/nvim/lua/plugins/testing.lua
+++ config/nvim/lua/plugins/testing.lua
@@ -10,8 +10,8 @@ return {
       -- adapters
       -- "Arthur944/neotest-bun",
       "fredrikaverpil/neotest-golang",
-      "haydenmeade/neotest-jest",
       "marilari88/neotest-vitest",
+      "nvim-neotest/neotest-jest",
       "olimorris/neotest-rspec",
     },
     config = function()
