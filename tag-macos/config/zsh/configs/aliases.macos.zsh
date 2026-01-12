aeaed0de17ed7b819796d5774a7c94194145efe7
Tom Bell <tomb@tombell.dev>
2026-01-12 23:45:28 +0000

    Add opencode alias

--- tag-macos/config/zsh/configs/aliases.macos.zsh
+++ tag-macos/config/zsh/configs/aliases.macos.zsh
@@ -6,3 +6,5 @@ alias bl="brew list -1 --formula"
 alias blc="brew list -1 --cask"
 alias bd="brew leaves | xargs brew deps --formula --for-each | sed \"s/^.*:/$(tput setaf 4)&$(tput sgr0)/\""
 alias bup="brew update && brew upgrade && brew cleanup -s --prune=all"
+
+alias oc="opencode"
