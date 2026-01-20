96decc93492c35f01c0feb3a11447500d2019f4e
Tom Bell <tomb@tombell.dev>
2026-01-20 21:15:45 +0000

    Remove work configuration

--- scripts/work.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-source "$(dirname "$0")/common.sh"
-
-"$(dirname "$0")/brew.sh"
-rcup_install macos work -- -S karabiner
-"$(dirname "$0")/defaults.sh"
-"$(dirname "$0")/mise.sh"
