3998438ef764d60027cca279304e89131ee8f4f8
Tom Bell <tomb@tombell.dev>
2026-01-24 11:41:18 +0000

    Add cat alias when bat is installed

--- config/zsh/configs/aliases.zsh
+++ config/zsh/configs/aliases.zsh
@@ -1,11 +1,15 @@
-alias l="ls -lAh"
-alias ll="ls -al"
-alias la="ls -A"
+alias l='ls -lAh'
+alias ll='ls -al'
+alias la='ls -A'
 
-alias e="nvim"
+alias e='nvim'
 
 alias path='echo $PATH | tr -s ":" "\n"'
 
 alias urepo='for d in *; do (cd $d && pwd && git pull && echo); done'
 alias brepo='for d in *; do (cd "$d" && pwd && git branch && echo); done'
 alias stashed='for d in *; do (cd "$d" && pwd && git stash list && echo); done'
+
+if command -v bat >/dev/null 2>&1; then
+  alias cat='bat --paging=never'
+fi
