e173badbbc55b4b3d8352f3ac8a01a3cd6c6849b
Tom Bell <tomb@tombell.dev>
2026-02-08 19:55:04 +0000

    Add local neovim bin directory if exists to path

--- config/zsh/configs/post/path.zsh
+++ config/zsh/configs/post/path.zsh
@@ -3,4 +3,8 @@ GOPATH=$HOME/.local/share/go
 PATH="$GOPATH/bin:$PATH"
 PATH="$HOME/.local/bin:$PATH"
 
+if [ -d "$HOME/.local/share/neovim/bin" ]; then
+  PATH="$HOME/.local/share/neovim/bin:$PATH"
+fi
+
 export -U GOPATH PATH
