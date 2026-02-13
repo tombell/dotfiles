7e66881179e0bf4d7bc9d7b41348dea481d231db
Tom Bell <tomb@tombell.dev>
2026-02-13 21:48:47 +0000

    Remove scripts, extract into tombell/laptop

--- scripts/brew.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-echo "==> Installing homebrew formulae and casks..."
-brew bundle --file "$(dirname "$0")/Brewfile"
--- scripts/linux.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-source "$(dirname "$0")/common.sh"
-
-rcup_install linux
-"$(dirname "$0")/mise.sh"
