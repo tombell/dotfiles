93ca992b08913a9bd9562936a13ffb53836bde08
Tom Bell <tomb@tombell.dev>
2026-02-22 23:14:20 +0000

    Add zsh-fast-syntax-highlighting plguin to macOS zsh config

--- tag-macos/config/zsh/configs/plugins.zsh
+++ tag-macos/config/zsh/configs/plugins.zsh
@@ -1,5 +1,6 @@
 plugins=(
   "/opt/homebrew/opt/zsh-autosuggestions/share/zsh-autosuggestions/zsh-autosuggestions.zsh"
+  "/opt/homebrew/opt/zsh-fast-syntax-highlighting/share/zsh-fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh"
 )
 
 for plugin in "${plugins[@]}"; do
