b155efbb230c6f5b65df0641dc18a8aef9bdc925
Tom Bell <tomb@tombell.dev>
2026-02-26 21:39:37 +0000

    Remove zsh plugins

--- tag-macos/config/zsh/configs/plugins.zsh
+++ tag-macos/config/zsh/configs/plugins.zsh
@@ -1,6 +1,5 @@
 plugins=(
   "/opt/homebrew/opt/zsh-autosuggestions/share/zsh-autosuggestions/zsh-autosuggestions.zsh"
-  "/opt/homebrew/opt/zsh-fast-syntax-highlighting/share/zsh-fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh"
 )
 
 for plugin in "${plugins[@]}"; do
