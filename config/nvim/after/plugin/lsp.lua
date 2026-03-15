24dfa98c1e0a06d8a41be53f7fa8e0203faf7259
Tom Bell <tomb@tombell.dev>
2026-03-15 12:26:29 +0000

    Remove lua_ls work around, now fixed in lazydev

--- config/nvim/after/plugin/lsp.lua
+++ config/nvim/after/plugin/lsp.lua
@@ -6,17 +6,6 @@ vim.lsp.config("gopls", {
   },
 })
 
--- NOTE: https://github.com/folke/lazydev.nvim/issues/136
-vim.lsp.config("lua_ls", {
-  settings = {
-    Lua = {
-      workspace = {
-        library = { vim.env.VIMRUNTIME },
-      },
-    },
-  },
-})
-
 vim.lsp.config("vtsls", {
   settings = {
     typescript = { preferences = { importModuleSpecifier = "non-relative" } },
