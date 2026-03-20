6c69f0245188e45844b146bf844b28215ef94a25
Tom Bell <tomb@tombell.dev>
2026-03-20 21:20:33 +0000

    Update ruby formatting autocmd

--- config/nvim/after/ftplugin/ruby.lua
+++ config/nvim/after/ftplugin/ruby.lua
@@ -1,5 +1,5 @@
 vim.api.nvim_create_autocmd("BufWritePre", {
-  group = vim.api.nvim_create_augroup("tombell-ruby-format", {}),
+  group = vim.api.nvim_create_augroup("tombell-ruby-format", { clear = true }),
   callback = function()
     vim.lsp.buf.format()
   end,
