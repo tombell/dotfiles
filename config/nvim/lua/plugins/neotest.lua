231d3c85f270a6684cece6b847ee0f43b8046d4a
Tom Bell <tomb@tombell.dev>
2026-03-20 21:20:33 +0000

    Use nvim api for getting the current file

--- config/nvim/lua/plugins/neotest.lua
+++ config/nvim/lua/plugins/neotest.lua
@@ -13,7 +13,7 @@ return {
       }
 
       -- stylua: ignore start
-      vim.keymap.set("n", "<leader>tt", function() require("neotest").run.run(vim.fn.expand("%")) end, { desc = "Run File" })
+      vim.keymap.set("n", "<leader>tt", function() require("neotest").run.run(vim.api.nvim_buf_get_name(0)) end, { desc = "Run File" })
       vim.keymap.set("n", "<leader>tT", function() require("neotest").run.run(vim.uv.cwd()) end, { desc = "Run All Test Files" })
       vim.keymap.set("n", "<leader>tr", function() require("neotest").run.run() end, { desc = "Run Nearest" })
       vim.keymap.set("n", "<leader>tl", function() require("neotest").run.run_last() end, { desc = "Run Last" })
@@ -21,7 +21,7 @@ return {
       vim.keymap.set("n", "<leader>to", function() require("neotest").output.open({ enter = true, auto_close = true }) end, { desc = "Show Output" })
       vim.keymap.set("n", "<leader>tO", function() require("neotest").output_panel.toggle() end, { desc = "Toggle Output Panel" })
       vim.keymap.set("n", "<leader>tS", function() require("neotest").run.stop() end, { desc = "Stop" })
-      vim.keymap.set("n", "<leader>tw", function() require("neotest").watch.toggle(vim.fn.expand("%")) end, { desc = "Toggle Watch" })
+      vim.keymap.set("n", "<leader>tw", function() require("neotest").watch.toggle(vim.api.nvim_buf_get_name(0)) end, { desc = "Toggle Watch" })
       -- stylua: ignore end
     end,
   },
