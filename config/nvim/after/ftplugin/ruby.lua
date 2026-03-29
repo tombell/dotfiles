1537ca1365cd49e1c0e47d89b16ad0f2c95e5409
Tom Bell <tomb@tombell.dev>
2026-03-29 12:26:43 +0100

    Update ruby ftplugin autocmd to match only ruby files

--- config/nvim/after/ftplugin/ruby.lua
+++ config/nvim/after/ftplugin/ruby.lua
@@ -1,5 +1,6 @@
 vim.api.nvim_create_autocmd("BufWritePre", {
   group = vim.api.nvim_create_augroup("tombell-ruby-format", { clear = true }),
+  pattern = "*.rb",
   callback = function()
     vim.lsp.buf.format()
   end,
