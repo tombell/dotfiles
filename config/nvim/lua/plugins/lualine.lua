a4e32c5d4406dd3f79c81b5aa566e318cfd2cdf3
Tom Bell <tomb@tombell.dev>
2026-04-02 20:25:31 +0100

    Remove lsp names from lualine

--- config/nvim/lua/plugins/lualine.lua
+++ config/nvim/lua/plugins/lualine.lua
@@ -5,7 +5,6 @@ return {
       require("lualine").setup {
         sections = {
           lualine_c = { { "filename", path = 1 } },
-          -- lualine_x = { { "lsp_status", icon = "" }, "encoding", "fileformat", "filetype" },
         },
       }
     end,
