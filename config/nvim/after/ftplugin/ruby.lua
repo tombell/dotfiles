f72ba621f3ba367fec4289ea4114ac14f1dc8fd5
Tom Bell <tomb@tombell.dev>
2026-05-16 10:21:32 +0100

    Remove unused ftplugin for ruby

--- config/nvim/after/ftplugin/ruby.lua
+++
@@ -1,7 +0,0 @@
-vim.api.nvim_create_autocmd("BufWritePre", {
-  group = vim.api.nvim_create_augroup("tombell-ruby-format", { clear = true }),
-  pattern = "*.rb",
-  callback = function()
-    vim.lsp.buf.format()
-  end,
-})
