c865cd584989907b304536782e1eb74da78037af
Tom Bell <tomb@tombell.dev>
2026-05-19 00:17:53 +0100

    Move mise activate into path.zsh

--- config/zsh/configs/post/path.zsh
+++ config/zsh/configs/post/path.zsh
@@ -1,10 +1,14 @@
 GOPATH=$HOME/.local/share/go
 
-PATH="$GOPATH/bin:$PATH"
 PATH="$HOME/.local/bin:$PATH"
+PATH="$GOPATH/bin:$PATH"
 
 if [ -d "$HOME/.local/share/neovim/bin" ]; then
   PATH="$HOME/.local/share/neovim/bin:$PATH"
 fi
 
 export -U GOPATH PATH
+
+if command -v mise >/dev/null 2>&1; then
+  eval "$(mise activate zsh)"
+fi
