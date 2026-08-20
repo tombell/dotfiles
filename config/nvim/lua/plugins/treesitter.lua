fc50d9b3fa0020ba2de7c6b1a8ac4333c935c71b
Tom Bell <tomb@tombell.dev>
2026-08-20 14:20:50 +0100

    Switch back to nvim-treesitter

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -1,81 +1,98 @@
 return {
-  src = "https://github.com/arborist-ts/arborist.nvim",
+  src = "https://github.com/nvim-treesitter/nvim-treesitter",
   data = {
     setup = function()
-      local arborist = require "arborist"
-      local plugin_root = vim.fn.fnamemodify(debug.getinfo(arborist.setup, "S").source:sub(2), ":h:h:h")
+      vim.api.nvim_create_autocmd("User", {
+        pattern = "TSUpdate",
+        callback = function()
+          local parsers = require "nvim-treesitter.parsers"
 
-      -- Neovim's runtimepath prepend no longer moves an existing entry, so
-      -- remove Arborist first to ensure its curated queries precede generated ones.
-      vim.opt.runtimepath:remove(plugin_root)
-      vim.opt.runtimepath:prepend(plugin_root)
+          ---@diagnostic disable: missing-fields
+          parsers.applescript = {
+            install_info = {
+              url = "https://github.com/waddie/tree-sitter-applescript",
+              queries = "queries",
+            },
+          }
 
-      arborist.setup {
-        prefer_wasm = false,
-        update_cadence = "manual",
-        ensure_installed = {
-          "applescript",
-          "bash",
-          "c",
-          "cpp",
-          "css",
-          "desktop",
-          "diff",
-          "dockerfile",
-          "editorconfig",
-          "embedded_template",
-          "git_config",
-          "git_rebase",
-          "gitcommit",
-          "gitignore",
-          "go",
-          "gomod",
-          "gosum",
-          "gotmpl",
-          "gowork",
-          "graphql",
-          "html",
-          "hyprlang",
-          "ini",
-          "javascript",
-          "jsdoc",
-          "json",
-          "latex",
-          "liquid",
-          "lua",
-          "luadoc",
-          "make",
-          "markdown",
-          "markdown_inline",
-          "objc",
-          "python",
-          "qmljs",
-          "query",
-          "regex",
-          "ruby",
-          "rust",
-          "skbdrc",
-          "sql",
-          "ssh_config",
-          "swift",
-          "tmux",
-          "toml",
-          "tsx",
-          "typescript",
-          "vimdoc",
-          "xml",
-          "yaml",
-          "zig",
-          "zsh",
-        },
-        overrides = {
-          applescript = {
-            url = "https://github.com/waddie/tree-sitter-applescript",
-          },
-          skbdrc = {
-            url = "https://github.com/starkwm/tree-sitter-skbdrc",
-          },
-        },
+          parsers.skbdrc = {
+            install_info = {
+              url = "https://github.com/starkwm/tree-sitter-skbdrc",
+              queries = "queries/skbdrc",
+            },
+          }
+          ---@diagnostic enable: missing-fields
+        end,
+      })
+
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = "*",
+        callback = function()
+          if pcall(vim.treesitter.start) then
+            vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+            vim.wo.foldmethod = "expr"
+          end
+        end,
+      })
+
+      require("nvim-treesitter").setup()
+      require("nvim-treesitter").install {
+        "bash",
+        "c",
+        "cpp",
+        "css",
+        "desktop",
+        "diff",
+        "dockerfile",
+        "editorconfig",
+        "embedded_template",
+        "git_config",
+        "git_rebase",
+        "gitcommit",
+        "gitignore",
+        "go",
+        "gomod",
+        "gosum",
+        "gotmpl",
+        "gowork",
+        "graphql",
+        "html",
+        "hyprlang",
+        "ini",
+        "javascript",
+        "jsdoc",
+        "json",
+        "jsx",
+        "latex",
+        "liquid",
+        "lua",
+        "luadoc",
+        "make",
+        "markdown",
+        "markdown_inline",
+        "objc",
+        "python",
+        "qmljs",
+        "query",
+        "regex",
+        "ruby",
+        "rust",
+        "sql",
+        "ssh_config",
+        "swift",
+        "tmux",
+        "toml",
+        "tsx",
+        "typescript",
+        "vimdoc",
+        "xml",
+        "yaml",
+        "zig",
+        "zsh",
+
+        -- external
+        "applescript",
+        "skbdrc",
       }
     end,
   },
