d10fcdec3b49b3e767802f73e77a2a568ee8ec13
Tom Bell <tomb@tombell.dev>
2026-08-20 20:16:37 +0100

    Update to disable homebrew hints

--- tag-macos/config/fish/conf.d/05-homebrew.fish
+++ tag-macos/config/fish/conf.d/05-homebrew.fish
@@ -1,5 +1,6 @@
 set -gx HOMEBREW_NO_ASK 1
 set -gx HOMEBREW_NO_UPGRADE_AUTO_UPDATES_CASKS 1
+set -gx HOMEBREW_NO_ENV_HINTS 1
 
 if test -x /opt/homebrew/bin/brew
     /opt/homebrew/bin/brew shellenv fish | source
