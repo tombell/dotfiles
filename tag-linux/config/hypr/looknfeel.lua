44bad9e5099b21a2628ba984b4c18aca9954b6c9
Tom Bell <tomb@tombell.dev>
2026-09-18 11:43:38 +0100

    Update hyprland gaps

--- tag-linux/config/hypr/looknfeel.lua
+++ tag-linux/config/hypr/looknfeel.lua
@@ -1,7 +1,7 @@
 hl.config {
   general = {
-    gaps_in = 10,
-    gaps_out = 20,
+    gaps_in = 5,
+    gaps_out = 10,
 
     border_size = 1,
 
@@ -13,7 +13,7 @@ hl.config {
       inactive_border = "rgba(65,72,104,0.666)",
     },
 
-    resize_on_border = true,
+    resize_on_border = false,
   },
 
   decoration = {
