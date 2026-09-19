a39449c72060493eb92acd20b1e9c94e7d5cf524
Tom Bell <tomb@tombell.dev>
2026-09-19 11:34:55 +0100

    Update fish aliases

--- config/fish/conf.d/20-aliases.fish
+++ config/fish/conf.d/20-aliases.fish
@@ -1,17 +1,17 @@
 status --is-interactive; or return
 
-# alias l='ls -lAh'
-# alias ll='ls -al'
-# alias la='ls -A'
+# alias l 'ls -lAh'
+# alias ll 'ls -al'
+# alias la 'ls -A'
 
-alias l='eza -lAg' # matches ls -lAh: excludes . and ..
-alias ll='eza -laag' # matches ls -al: includes . and ..
-alias la='eza -A' # matches ls -A: excludes . and ..
+alias l 'eza -lAg' # matches ls -lAh: excludes . and ..
+alias ll 'eza -laag' # matches ls -al: includes . and ..
+alias la 'eza -A' # matches ls -A: excludes . and ..
 
-alias tree='eza -A --tree'
+alias tree 'eza -A --tree'
 
-alias ..='cd ..'
-alias ...='cd ../..'
+alias .. 'cd ..'
+alias ... 'cd ../..'
 
 if type -q bat
     alias cat='bat --paging=never'
@@ -20,3 +20,5 @@ end
 if type -q mise
     alias mup='mise upgrade'
 end
+
+alias ppath 'printf "%s\n" $PATH'
