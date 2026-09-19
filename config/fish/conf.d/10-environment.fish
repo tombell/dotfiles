a8afbff9ca413c830f2e7ed457be4a2883cff784
Tom Bell <tomb@tombell.dev>
2026-09-19 23:44:06 +0100

    Add tokyonight eza theme

--- config/fish/conf.d/10-environment.fish
+++ config/fish/conf.d/10-environment.fish
@@ -6,6 +6,8 @@ set -gx EDITOR $VISUAL
 set -gx CLICOLOR 1
 set -gx GOPATH "$HOME/.local/share/go"
 
+set -gx EZA_CONFIG_DIR "$XDG_CONFIG_HOME/eza"
+
 fish_add_path --move --path \
     "$HOME/.local/share/neovim/bin" \
     "$GOPATH/bin" \
