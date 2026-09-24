03c19d99c5626173029a092afec15bb37b5a35bd
Tom Bell <tomb@tombell.dev>
2026-09-24 09:47:45 +0100

    Update to only install nil lsp when nix is available

--- config/nvim/lua/plugins/mason-tool-installer.lua
+++ config/nvim/lua/plugins/mason-tool-installer.lua
@@ -2,22 +2,25 @@ return {
   src = "https://github.com/WhoIsSethDaniel/mason-tool-installer.nvim",
   data = {
     setup = function()
-      require("mason-tool-installer").setup {
-        ensure_installed = {
-          "alejandra",
-          "gofumpt",
-          "goimports",
-          "gopls",
-          "json-lsp",
-          "lua-language-server",
-          "nil",
-          "oxfmt",
-          "oxlint",
-          "shfmt",
-          "stylua",
-          "vtsls",
-        },
+      local ensure_installed = {
+        "alejandra",
+        "gofumpt",
+        "goimports",
+        "gopls",
+        "json-lsp",
+        "lua-language-server",
+        "oxfmt",
+        "oxlint",
+        "shfmt",
+        "stylua",
+        "vtsls",
       }
+
+      if vim.fn.executable "nix" == 1 then
+        table.insert(ensure_installed, "nil")
+      end
+
+      require("mason-tool-installer").setup { ensure_installed = ensure_installed }
     end,
   },
 }
